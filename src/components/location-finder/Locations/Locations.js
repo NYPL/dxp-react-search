399816ac32dcb5ef513aa299712dd5b21afe63a0
wluisi <w.luisi@williamluisi.org>
2020-07-15 15:00:28 -0400

    rough working draft of adding a basic google map

--- src/components/location-finder/Locations/Locations.js
+++ src/components/location-finder/Locations/Locations.js
@@ -1,38 +1,18 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
-import SearchBar from './../SearchBar';
 import Location from './../Location';
 import { LocationsQuery as LOCATIONS_QUERY } from './Locations.gql';
 import styles from './Locations.module.css';
-
 // Redux
 import { useSelector } from 'react-redux';
 
 function Locations() {
   const { loading, error, data, networkStatus } = useQuery(
-    LOCATIONS_QUERY,
-    {
-      //notifyOnNetworkStatusChange: false,
-      //fetchPolicy: 'no-cache',
-    }
+    LOCATIONS_QUERY, {}
   );
-
-  const searchQuery = useSelector(state => state.search.searchQuery);
-  console.log('searchQuery state: ' + searchQuery);
-
-  const searchQueryGeoLat = useSelector(state => state.search.searchQueryGeoLat);
-  console.log('searchQueryGeoLat state: ' + searchQueryGeoLat);
-
-  const searchQueryGeoLng = useSelector(state => state.search.searchQueryGeoLng);
-  console.log('searchQueryGeoLng state: ' + searchQueryGeoLng);
-
-
-
-  /*console.log('loading: ' + loading);
-  console.log('error: ' + error);
-  console.log('data: ' + data);
-  */
+  // Redux
+  const { searchQuery, searchQueryGeoLat, searchQueryGeoLng } = useSelector(state => state.search);
 
   if (loading || !data) {
     return (
@@ -48,10 +28,9 @@ function Locations() {
 
   return (
     <div>
-      <SearchBar />
       {searchQuery ? (
         <div>
-          Showing 10 locations near <strong>{searchQuery}</strong>
+          Showing all locations near <strong>{searchQuery}</strong>
           <br />
           <div>searchQueryGeoLat: {searchQueryGeoLat}</div>
           <div>searchQueryGeoLng: {searchQueryGeoLng}</div>
--- src/pages/location-finder/index.js
+++ src/pages/location-finder/index.js
@@ -6,12 +6,30 @@ import { compose } from 'redux';
 // Components
 import Layout from './../../components/shared/layouts/Main';
 import Locations from './../../components/location-finder/Locations/Locations';
+import Map from './../../components/location-finder/Map';
+import SearchBar from './../../components/location-finder/SearchBar';
+const { NEXT_PUBLIC_GOOGLE_MAPS_API } = process.env;
 
 function LocationFinder() {
   return (
     <Layout>
       <h1>Location Finder</h1>
-      <Locations />
+      <SearchBar />
+      <div className='location-finder-wrapper'>
+        <div className='row'>
+          <div className='column'>
+            <Locations />
+          </div>
+          <div className='column'>
+            <Map
+              googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key= ${NEXT_PUBLIC_GOOGLE_MAPS_API}`}
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: `400px` }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+            />
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
