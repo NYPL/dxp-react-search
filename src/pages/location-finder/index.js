9ce665f85ca31850545b8ac99b6373fa0767d402
wluisi <w.luisi@williamluisi.org>
2020-07-23 16:18:17 -0400

    restructured components to use SearchHeader and SearchForm

--- src/pages/location-finder/index.js
+++ src/pages/location-finder/index.js
@@ -5,14 +5,14 @@ import { withRedux } from './../../redux/withRedux';
 import { compose } from 'redux';
 // Components
 import Layout from './../../components/shared/layouts/Main';
-import SearchBar from './../../components/location-finder/SearchBar';
+import SearchHeader from './../../components/location-finder/SearchHeader';
 import Locations from './../../components/location-finder/Locations/Locations';
 
 function LocationFinder() {
   return (
     <Layout>
       <div className="content-header">
-        <SearchBar />
+        <SearchHeader />
       </div>
       <div className="content-primary">
         <Locations />
