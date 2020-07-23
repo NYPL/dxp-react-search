819a98f7018d61545e0a10a3d50763cf3781dc6b
wluisi <w.luisi@williamluisi.org>
2020-07-23 12:54:44 -0400

    second draft of scss

--- src/pages/location-finder/index.js
+++ src/pages/location-finder/index.js
@@ -5,31 +5,14 @@ import { withRedux } from './../../redux/withRedux';
 import { compose } from 'redux';
 // Components
 import Layout from './../../components/shared/layouts/Main';
-import Locations from './../../components/location-finder/Locations/Locations';
-import Map from './../../components/location-finder/Map';
 import SearchBar from './../../components/location-finder/SearchBar';
-const { NEXT_PUBLIC_GOOGLE_MAPS_API } = process.env;
+import Locations from './../../components/location-finder/Locations/Locations';
 
 function LocationFinder() {
   return (
     <Layout>
-      <h1>Location Finder</h1>
       <SearchBar />
-      <div className='location-finder-wrapper'>
-        <div className='row'>
-          <div className='column locations-list'>
-            <Locations />
-          </div>
-          <div className='column'>
-            <Map
-              googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${NEXT_PUBLIC_GOOGLE_MAPS_API}`}
-              loadingElement={<div style={{ height: `100%` }} />}
-              containerElement={<div style={{ height: `500px` }} />}
-              mapElement={<div style={{ height: `100%` }} />}
-            />
-          </div>
-        </div>
-      </div>
+      <Locations />
     </Layout>
   );
 }
