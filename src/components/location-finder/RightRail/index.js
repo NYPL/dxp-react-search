4ec85542e7baab6ece83a9f1d65784728184ce9b
Milos Pumpalovic <knezmilos@gmail.com>
2020-07-30 12:15:58 -0400

    RENO-1549 - changed design system react components version to 0.7.2, updated "Link" component markup

--- src/components/location-finder/RightRail/index.js
+++ src/components/location-finder/RightRail/index.js
@@ -24,24 +24,24 @@ function RightRail() {
             type="ul"
           >
           <li className="right-rail__item">
-            <DS.Link linkType="default" className="right-rail__link">
+            <DS.Link className="right-rail__link">
               <a href="https://www.nypl.org/library-card">
                 Get a Library Card
               </a>
             </DS.Link>
           </li>          
           <li className="right-rail__item">
-            <DS.Link linkType="default" className="right-rail__link">
+            <DS.Link className="right-rail__link">
               <a href="https://www.nypl.org/books-more/recommendations/staff-picks/adults">Find Your Next Book</a>
             </DS.Link>
           </li>          
           <li className="right-rail__item">
-            <DS.Link linkType="default" className="right-rail__link">
+            <DS.Link className="right-rail__link">
               <a href="https://www.nypl.org/locations">Search Library Locations</a>
             </DS.Link>
           </li>     
           <li className="right-rail__item">
-            <DS.Link linkType="default" className="right-rail__link">
+            <DS.Link className="right-rail__link">
               <a href="https://www.nypl.org/help/computers-internet-and-wireless-access/reserving-computer">Reserve a Computer</a>
             </DS.Link>
           </li>
@@ -58,12 +58,12 @@ function RightRail() {
 
         <ul className="right-rail__list">
           <li className="right-rail__item">
-            <DS.Link linkType="default" className="right-rail__link">
+            <DS.Link className="right-rail__link">
               <a href="https://www.nypl.org/email-us">Email us your question</a>
             </DS.Link>
           </li>          
           <li className="right-rail__item">
-            <DS.Link linkType="default" className="right-rail__link">
+            <DS.Link className="right-rail__link">
               <a href="https://www.nypl.org/get-help/contact-us/chat">Chat with a librarian</a>
             </DS.Link>
           </li>
@@ -92,12 +92,12 @@ function RightRail() {
         
         <ul className="right-rail__list">      
           <li className="right-rail__item">
-            <DS.Link linkType="default" className="right-rail__link">
+            <DS.Link className="right-rail__link">
               <a href="https://www.nypl.org/help/about-nypl/volunteer-nypl">Volunteer</a>
             </DS.Link>
           </li>                
           <li className="right-rail__item right-rail__item--button">                       
-            <DS.Link linkType="default" className="action-link">
+            <DS.Link className="action-link">
               <a href="https://secure.nypl.org/site/Donation2?7825_donation=form1&amp;df_id=7825&amp;mfc_pref=T&amp;set_custom_Donation_Direction=Mid-Manhattan%20at%2042nd%20Street%20Library&amp;s_src=FRQXXZZ_QWLPN">Support Your Library</a>
             </DS.Link>
           </li>
