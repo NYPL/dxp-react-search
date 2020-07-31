10d1ca3a0b440df29c313288c8e3b2d47eb7e261
wluisi <w.luisi@williamluisi.org>
2020-07-31 11:36:00 -0400

    code formatting cleanup

--- src/components/location-finder/RightRail/index.js
+++ src/components/location-finder/RightRail/index.js
@@ -9,17 +9,14 @@ function RightRail() {
   const { more_at_nypl, need_help, support_nypl } = RightRailContent;
 
   return (
-
     <div className="bottom-rails">
       <nav className="right-rail" aria-labelledby="right-rail--more-at-nypl">
-
         <DS.Heading
           className="right-rail__heading"
           id="right-rail--more-at-nypl"
           level={3}
           text="More at NYPL"
         />
-        
         <DS.List
             className="right-rail__list"
             modifiers={[
@@ -28,11 +25,17 @@ function RightRail() {
             type="ul"
           >
           {more_at_nypl.map((value, index) => {
-            return <li className="right-rail__item"><DS.Link className="right-rail__link" href={value.link}>{value.title}</DS.Link></li>
+            return (
+              <li className="right-rail__item">
+                <DS.Link
+                  className="right-rail__link"
+                  href={value.link}>{value.title}
+                </DS.Link>
+              </li>
+            );
           })}
         </DS.List>
       </nav>
-
       <nav className="right-rail" aria-labelledby="right-rail--need-help?-ask-nypl">
         <DS.Heading
           id="right-rail--need-help?-ask-nypl"
@@ -40,24 +43,30 @@ function RightRail() {
           text="Need Help? Ask NYPL"
           className="right-rail__heading"
         />
-
         <DS.List
-            className="right-rail__list"
-            modifiers={[
-              'no-list-styling'
-            ]}
-            type="ul"
-          >
+          className="right-rail__list"
+          modifiers={[
+            'no-list-styling'
+          ]}
+          type="ul"
+        >
           {need_help.map((value, index) => {
             if(value.link ) {
-              return <li className="right-rail__item"><DS.Link className="right-rail__link" href={value.link}>{value.title}</DS.Link></li>
+              return (
+                <li className="right-rail__item">
+                  <DS.Link className="right-rail__link" href={value.link}>{value.title}</DS.Link>
+                </li>
+              );
             } else {
-              return <li className="right-rail__item"><span className="right-rail__link">{value.title}</span></li>
+              return (
+                <li className="right-rail__item">
+                  <span className="right-rail__link">{value.title}</span>
+                </li>
+              );
             }
           })}
         </DS.List>
       </nav>
-
       <nav className="right-rail" aria-labelledby="right-rail--support-nypl">
         <DS.Heading
           className="right-rail__heading"
@@ -65,19 +74,26 @@ function RightRail() {
           level={3}
           text="Support NYPL"
         />
-        
         <DS.List
-            className="right-rail__list"
-            modifiers={[
-              'no-list-styling'
-            ]}
-            type="ul"
-          >
+          className="right-rail__list"
+          modifiers={[
+            'no-list-styling'
+          ]}
+          type="ul"
+        >
           {support_nypl.map((value, index) => {
             if(value.type ) {
-              return <li className="right-rail__item"><DS.Link linkType="button" className="action-link" href={value.link}>{value.title}</DS.Link></li>
+              return (
+                <li className="right-rail__item">
+                  <DS.Link linkType="button" className="action-link" href={value.link}>{value.title}</DS.Link>
+                </li>
+              );
             } else {
-              return <li className="right-rail__item"><span className="right-rail__link">{value.title}</span></li>
+              return (
+                <li className="right-rail__item">
+                  <span className="right-rail__link">{value.title}</span>
+                </li>
+              );
             }
           })}
         </DS.List>
