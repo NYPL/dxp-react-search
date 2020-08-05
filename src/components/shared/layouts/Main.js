049e32479de29ae8456fd0578e8b2de7b4a0f159
Matt Misselbeck <matt@misselbeck.com>
2020-08-05 11:01:11 -0400

    RENO-1550 build breadcrumb component (#10)
    * RENO-1550: Update Design System to v0.9.1
    
    * RENO-1550: Breadcrumb component and base class
    
    * RENO-1550: Update package-lock.json

--- src/components/shared/layouts/Main.js
+++ src/components/shared/layouts/Main.js
@@ -1,11 +1,20 @@
 import { Header, navConfig } from '@nypl/dgx-header-component';
+import * as DS from '@nypl/design-system-react-components';
 import Footer from '@nypl/dgx-react-footer';
 
 export default ({ children }) => (
-  <div className="layout-container">
+  <div className="layout-container nypl--locations">
     <Header
       skipNav={{ target: 'main-content' }}
       navData={navConfig.current}
+    />
+    <DS.Breadcrumbs
+      breadcrumbs={[
+        {
+          text: 'Home',
+          url: 'https://www.nypl.org/'
+        }
+      ]}
     />
     <main className="main">
       {children}
