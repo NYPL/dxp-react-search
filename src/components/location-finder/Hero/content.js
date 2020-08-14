3a419fa36c5f7cecf32c62231b5353b796439322
Milos Pumpalovic <knezmilos@gmail.com>
2020-08-13 21:13:44 -0400

    RENO-1601 - Update hero text

--- src/components/location-finder/Hero/content.js
+++ src/components/location-finder/Hero/content.js
@@ -1,5 +1,5 @@
 const HeroContent = {
-  'text': 'With 92 locations across the Bronx, Manhattan, and Staten Island, The New York Public Library is an essential part of neighborhoods across the city. Visit us today.',
+  'text': 'Please note that as part of our gradual reopening, select branches are currently open with limited grab-and-go service, while others remain temporarily closed. <a href="https://www.nypl.org/locations/directory">Learn more about which locations are open and available services.</a>',
 };
 
 export default HeroContent;
