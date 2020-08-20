d791d536e88dd009745df2cc445909d9af7a9651
wluisi <w.luisi@williamluisi.org>
2020-08-20 13:43:00 -0400

    added next-transpile-modules and updated next.config.js to transpile DS for ie11 compatibility

--- next.config.js
+++ next.config.js
@@ -1,21 +1,20 @@
-module.exports = (nextConfig = {}) => {
-  return Object.assign({}, nextConfig, {
-    webpack(config, options) {
-      const { dir } = options
+// Special handling for nypl design system to be ie11 compatiable.
+const withTM = require('next-transpile-modules')(['@nypl/design-system-react-components']);
 
-      // Allows import of .gql files inside components
-      config.module.rules.push({
-        test: /\.(graphql|gql)$/,
-        include: [dir],
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'graphql-tag/loader'
-          }
-        ]
-      })
-
-      return config;
-    }
-  })
-}
+module.exports = withTM({
+  webpack: (config, options) => {
+    const { dir } = options
+    // Allows import of .gql files inside components
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      include: [dir],
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'graphql-tag/loader'
+        }
+      ]
+    })
+    return config;
+  }
+});
