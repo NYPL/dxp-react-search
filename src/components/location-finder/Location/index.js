0d1a45b96225bf1292725750090b6b65f43a091f
knezmilos <knezmilos@gmail.com>
2020-08-20 12:50:06 -0400

    RENO-1618 - Location Finder VQA (#29)
    Includes fixes from the following Jira tickets:
    https://jira.nypl.org/browse/RENO-1618
    https://jira.nypl.org/browse/RENO-1619
    https://jira.nypl.org/browse/RENO-1621
    https://jira.nypl.org/browse/RENO-1623
    https://jira.nypl.org/browse/RENO-1624
    https://jira.nypl.org/browse/RENO-1626
    https://jira.nypl.org/browse/RENO-1628
    https://jira.nypl.org/browse/RENO-1629

--- src/components/location-finder/Location/index.js
+++ src/components/location-finder/Location/index.js
@@ -11,7 +11,7 @@ function Location({ location }) {
   const dispatch = useDispatch();
 
   // Address formatting.
-  const formattedAddress = `${location.address_line1} ${location.locality}, ${location.administrative_area} ${location.postal_code}`;
+  const formattedAddress = `${location.address_line1}\n${location.locality}, ${location.administrative_area} ${location.postal_code}`;
   // Get directions link.
   const encodedAddress = encodeURIComponent(formattedAddress);
   const getDirectionsLink = 'http://maps.google.com/maps?f=q&hl=en&saddr=&daddr=' + encodedAddress;
