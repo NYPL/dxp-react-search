25fb2de1198b083a61a8592697f4adb247ce679a
Matt Misselbeck <matt@misselbeck.com>
2020-08-24 11:40:38 -0400

    RENO-1616: Metatags (#36)
    * RENO-1616: Metatags
    
    * RENO-1616: Meta component

--- src/pages/location-finder/index.js
+++ src/pages/location-finder/index.js
@@ -4,23 +4,20 @@ import { withApollo } from './../../apollo/client/withApollo';
 import { withRedux } from './../../redux/withRedux';
 import { compose } from 'redux';
 // Components
+import Meta from './../../components/location-finder/Meta';
 import Layout from './../../components/shared/layouts/Main';
 import Hero from './../../components/location-finder/Hero';
 import SearchHeader from './../../components/location-finder/SearchHeader';
 import Locations from './../../components/location-finder/Locations/Locations';
 import SearchResultsDetails from './../../components/location-finder/SearchResultsDetails';
 import Map from './../../components/location-finder/Map';
-import Head from 'next/head';
 import BottomPromo from '../../components/location-finder/BottomPromo';
 import RightRail from '../../components/location-finder/RightRail';
 
 function LocationFinder() {
   return (
     <Layout>
-      <Head>
-        <title>Location Finder</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
+      <Meta />
       <div className="content-header">
         <Hero />
         <SearchHeader />
