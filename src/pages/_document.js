891b938a89cbf18e5e1e8ebaf3e7af69a58ce8da
wluisi <w.luisi@williamluisi.org>
2020-08-25 17:23:48 -0400

    cleanup for _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,22 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-class SearchDocument extends Document {
-	static async getInitialProps(ctx) {
-		const initialProps = await Document.getInitialProps(ctx)
-		return { ...initialProps }
-	}
-
-	render() {
-		return (
-			<Html lang="en">
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		)
-	}
+class ScoutDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default SearchDocument
+export default ScoutDocument;
