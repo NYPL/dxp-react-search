b391a7b009e455980392942b9dc037123fc56abd
wluisi <w.luisi@williamluisi.org>
2020-08-26 16:55:18 -0400

    removed unecessary analytics.js file in utils

--- src/utils/analytics.js
+++
@@ -1,9 +0,0 @@
-// @TODO Might not need this file?
-const { NEXT_PUBLIC_GA_TRACKING_ID } = process.env;
-
-// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = (url) => {
-  window.gtag('config', NEXT_PUBLIC_GA_TRACKING_ID, {
-    page_path: url,
-  })
-}
