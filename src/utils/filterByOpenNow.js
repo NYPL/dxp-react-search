00d9e14b5fef0fa82a41b8f679448b1bc48b4c7a
nypl-wluisi <62392174+nypl-wluisi@users.noreply.github.com>
2020-08-31 17:01:53 -0400

    RENO-1677: Open Now Filter Shows Locations That Are Closed (#43)
    * update filterByOpen Now to include alerts closings
    
    * cleanup and additional handling of alerts closings in filterByOpenNow util function
    
    * cleanup for refinery data source and removed ?page[size]=300 from refinery endpoint
    
    * rough draft of alert start date
    
    * almost there
    
    * fixes for checking alerts start and end dates
    
    Co-authored-by: wluisi <w.luisi@williamluisi.org>

--- src/utils/filterByOpenNow.js
+++ src/utils/filterByOpenNow.js
@@ -1,5 +1,9 @@
+import checkAlertsOpenStatus from './checkAlertsOpenStatus';
+
 function filterByOpenNow(locations) {
+  const tz = 'America/New_York';
   const today = new Date();
+
   // Get the current time, in format 13:56
   // Force timezone to new york.
   const nowTime = today.toLocaleTimeString('en-US', {
@@ -16,6 +20,9 @@ function filterByOpenNow(locations) {
   });
 
   return locations.reduce((accumlator, location) => {
+    // Alerts
+    const alertsOpenStatus = checkAlertsOpenStatus(location);
+
     location.hours.regular.map(hoursItem => {
       // Find today in weekly hours.
       if (hoursItem.day.replace('.','') === weekday) {
@@ -23,21 +30,14 @@ function filterByOpenNow(locations) {
         if (
           // Check for not null.
           hoursItem.open !== null && hoursItem.close !== null
-          // @TODO Check alerts: location._embedded.alerts for closings.
+          // Check for alert closings.
+          && alertsOpenStatus
           // Check for extended closing.
           && location.open
           // Check open/closed hours against now time.
           && hoursItem.open <= nowTime && hoursItem.close >= nowTime
         ) {
-          // @TODO Remove debug code.
-          /*
-          console.log(location.name);
-          console.log('location open hours: ' + hoursItem.open);
-          console.log('location close hours: ' + hoursItem.close);
-          console.log('now time: ' + nowTime);
-          console.log(typeof nowTime);
-          console.log(typeof hoursItem.close);
-          */
+          // Add location as open to accumulator.
           accumlator.push(location);
         }
       }
