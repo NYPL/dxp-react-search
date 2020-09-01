18ad99ce2184366b4bbcaf3d6959f03acb83c30d
wluisi <w.luisi@williamluisi.org>
2020-09-01 12:36:00 -0400

    cleanup for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Link from 'next/link';
 
 function Home() {
   return (
-    <pre>
-      OKAY
-    </pre>
+    <div>
+      <pre>
+        OKAY
+      </pre>
+    </div>
   );
 }
 
