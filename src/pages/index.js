5313009ec8aec4d32d44179ab332226ebc1be278
Greg Kallenberg <gregorykallenberg@nypl.org>
2020-09-01 17:22:56 -0400

    RENO-1681 & 1682 (#51)
    * chg to /locations for location-finder; add robots.txt; chg homepage to
    use healthcheck style output
    
    * move robots.txt to /public
    
    * cleanup for homepage
    
    Co-authored-by: wluisi <w.luisi@williamluisi.org>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import Link from 'next/link';
 
 function Home() {
   return (
     <div>
-      <h1>Search App</h1>
-      <Link href="/location-finder">
-        <a>Location Finder</a>
-      </Link>
-      <br />
-      <Link href="/events-search">
-        <a>Events Search</a>
-      </Link>
+      <pre>
+        OKAY
+      </pre>
     </div>
   );
 }
