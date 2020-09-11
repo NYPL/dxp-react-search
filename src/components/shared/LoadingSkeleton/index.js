8e2d0f5b0f81df450e2b6ea088adab6d845f2f46
wluisi <w.luisi@williamluisi.org>
2020-09-11 14:51:18 -0400

    code cleanup

--- src/components/shared/LoadingSkeleton/index.js
+++ src/components/shared/LoadingSkeleton/index.js
@@ -1,9 +1,1 @@
-import React from 'react';
-
-function LoadingSkeleton() {
-  return (
-    <div className='loading-skeleton'></div>
-  );
-};
-
-export default LoadingSkeleton;
+export { default } from './LoadingSkeleton';
