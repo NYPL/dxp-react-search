3c1a8d69f215943183859b8aa1af47e4021398a8
Matt Misselbeck <matt@misselbeck.com>
2020-09-23 16:17:34 -0400

    RENO-867: Add GTM Optimize config to the Google Analytics snippet

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,3 +1,4 @@
+const { NEXT_PUBLIC_GTM_TRACKING_ID } = process.env;
 const { NEXT_PUBLIC_GA_TRACKING_ID } = process.env;
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
@@ -17,8 +18,11 @@ class ScoutDocument extends Document {
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
+                gtag('require', '${NEXT_PUBLIC_GTM_TRACKING_ID}');
                 gtag('config', '${NEXT_PUBLIC_GA_TRACKING_ID}', {
                   page_path: window.location.pathname,
+                  'groups':'default',
+                  'anonymize_ip':true
                 });
               `,
             }}
