29e0f8d5949b7d77a67532e5755c0e4b742717d8
wluisi <w.luisi@williamluisi.org>
2020-10-09 16:26:08 -0400

    added HeroImage tests

---
+++ src/components/location-finder/HeroImage/HeroImage.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe';
+import HeroImage from './HeroImage';
+
+expect.extend(toHaveNoViolations);
+
+describe('HeroImage', () => {
+  test('renders HeroImage component', () => {
+    render(<HeroImage />);
+  });
+});
+
+it('should not have basic accessibility issues', async () => {
+  const { container } = render(<HeroImage />);
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
+});
