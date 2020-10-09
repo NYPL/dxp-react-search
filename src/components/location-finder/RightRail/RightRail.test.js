9d829fe5785397fd5cc0d65f69425b689d6d5ef5
wluisi <w.luisi@williamluisi.org>
2020-10-09 16:22:37 -0400

    added keys to map for right rail and added back tests

--- src/components/location-finder/RightRail/RightRail.test.js
+++ src/components/location-finder/RightRail/RightRail.test.js
@@ -5,30 +5,14 @@ import RightRail from './RightRail';
 
 expect.extend(toHaveNoViolations);
 
-// @TODO Remove this, only added so this file returns a test.
-describe('true is truthy and false is falsy', () => {
-  test('true is truthy', () => {
-    expect(true).toBe(true);
-  });
-
-  test('false is falsy', () => {
-    expect(false).toBe(false);
-  });
-});
-
-// @TODO This test will fail b/c missing key props
-/*describe('RightRail', () => {
+describe('RightRail', () => {
   test('renders RightRail component', () => {
     render(<RightRail />);
   });
 });
-*/
 
-// @TODO This test will fail right now, b/c of bug in design system Link component:
-// https://github.com/NYPL/nypl-design-system/issues/309
-/*it('should not have basic accessibility issues', async () => {
+it('should not have basic accessibility issues', async () => {
   const { container } = render(<RightRail />);
   const results = await axe(container);
   expect(results).toHaveNoViolations();
 });
-*/
