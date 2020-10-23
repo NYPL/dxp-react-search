274b73d9ef338c1af3bcdf7d420a1dec73013d43
wluisi <w.luisi@williamluisi.org>
2020-10-23 14:18:09 -0400

    cleanup for RightRail test

--- src/components/location-finder/RightRail/RightRail.test.js
+++ src/components/location-finder/RightRail/RightRail.test.js
@@ -9,10 +9,10 @@ describe('RightRail', () => {
   test('renders RightRail component', () => {
     render(<RightRail />);
   });
-});
 
-it('should not have basic accessibility issues', async () => {
-  const { container } = render(<RightRail />);
-  const results = await axe(container);
-  expect(results).toHaveNoViolations();
+  it('should not have basic accessibility issues', async () => {
+    const { container } = render(<RightRail />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
 });
