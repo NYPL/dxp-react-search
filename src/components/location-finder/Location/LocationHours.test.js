4c558a8280928d492d55df1d27c4760585eec526
nypl-wluisi <62392174+nypl-wluisi@users.noreply.github.com>
2020-11-03 14:24:25 -0500

    RENO-1827: Adds Tests for LocationHours Component (#78)
    * added rough draft of tests for LocationHours component
    
    * added hours and mins tests
    
    * cleanedup LocationHours test
    
    * fixed typo and added tests for start or end hours null
    
    * updated changelog
    
    Co-authored-by: wluisi <w.luisi@williamluisi.org>

---
+++ src/components/location-finder/Location/LocationHours.test.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from './../../../../testHelper/customRtlRender';
+import LocationHours from './LocationHours';
+
+expect.extend(toHaveNoViolations);
+
+describe('LocationHours Component', () => {
+  test('location should be open with regular hours.', () => {
+    render(
+      <LocationHours
+        open={true}
+        todayHoursStart={'11:00'}
+        todayHoursEnd={'12:00'}
+      />
+    );
+
+    expect(screen.getByText("Today's Hours:")).toBeInTheDocument();
+    expect(screen.getByText('11AM–12PM')).toBeInTheDocument();
+  });
+
+  test('location should be open with regular hours that include start mins', () => {
+    render(
+      <LocationHours
+        open={true}
+        todayHoursStart={'11:19'}
+        todayHoursEnd={'12:00'}
+      />
+    );
+
+    expect(screen.getByText("Today's Hours:")).toBeInTheDocument();
+    expect(screen.getByText('11:19AM–12PM')).toBeInTheDocument();
+  });
+
+  test('location should be open with regular hours that include end mins', () => {
+    render(
+      <LocationHours
+        open={true}
+        todayHoursStart={'11:00'}
+        todayHoursEnd={'12:57'}
+      />
+    );
+
+    expect(screen.getByText("Today's Hours:")).toBeInTheDocument();
+    expect(screen.getByText('11AM–12:57PM')).toBeInTheDocument();
+  });
+
+  test('location should be open with regular hours that include start and end mins', () => {
+    render(
+      <LocationHours
+        open={true}
+        todayHoursStart={'10:08'}
+        todayHoursEnd={'17:57'}
+      />
+    );
+
+    expect(screen.getByText("Today's Hours:")).toBeInTheDocument();
+    expect(screen.getByText('10:08AM–5:57PM')).toBeInTheDocument();
+  });
+
+  test('location should be closed due to regular hours', () => {
+    render(
+      <LocationHours
+        open={true}
+        todayHoursStart={null}
+        todayHoursEnd={null}
+      />
+    );
+
+    expect(screen.getByText('Closed.')).toBeInTheDocument();
+  });
+
+  test('location with regular hours should be temporarily closed due to extended closing or alert closing.', () => {
+    render(
+      <LocationHours
+        open={false}
+        todayHoursStart={'11:00'}
+        todayHoursEnd={'12:00'}
+      />
+    );
+
+    expect(screen.getByText('Location is temporarily closed')).toBeInTheDocument();
+  });
+
+  test('location without regular hours should be temporarily closed due to extended closing or alert closing. ', () => {
+    render(
+      <LocationHours
+        open={false}
+        todayHoursStart={null}
+        todayHoursEnd={null}
+      />
+    );
+
+    expect(screen.getByText('Location is temporarily closed')).toBeInTheDocument();
+  });
+
+  test('location with null for start hours only should be closed due to regular hours', () => {
+    render(
+      <LocationHours
+        open={true}
+        todayHoursStart={null}
+        todayHoursEnd={'12:00'}
+      />
+    );
+
+    expect(screen.getByText('Closed.')).toBeInTheDocument();
+  });
+
+  test('location with null for end hours only should be closed due to regular hours', () => {
+    render(
+      <LocationHours
+        open={true}
+        todayHoursStart={'11:15'}
+        todayHoursEnd={null}
+      />
+    );
+
+    expect(screen.getByText('Closed.')).toBeInTheDocument();
+  });
+
+  // Accessbiility tests.
+  it('should not have basic accessibility issues', async () => {
+    const { container } = render(
+      <LocationHours
+        open={true}
+        todayHoursStart={'11:00'}
+        todayHoursEnd={'18:00'}
+      />
+    );
+
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
