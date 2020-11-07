e5f4eb1759605a8642383efe0f7b1eca8b3c0e00
wluisi <w.luisi@williamluisi.org>
2020-11-06 20:49:59 -0500

    added new files

---
+++ src/utils/setTodaysHours.js
@@ -0,0 +1,89 @@
+const dayjs = require('dayjs');
+var isBetween = require('dayjs/plugin/isBetween');
+dayjs.extend(isBetween);
+
+/**
+ * Set today's hours using regular hours or modified hours using alerts closings.
+ *
+ * @param {object} now - current date object, unformatted.
+ * @param {object} regularHours - an object of regular hours data.
+ * @param {object} alerts - an object of alerts & closings data.
+ * @return {object} todaysHours - an object of start and end hours for today.
+ */
+function setTodaysHours(now, regularHours, alerts, alertsOpenStatus) {
+  // Today hours
+  const weekDayKeys = new Array('Sun.', 'Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.');
+
+  let todayHoursStart;
+  let todayHoursEnd;
+
+  regularHours.map(item => {
+    if (weekDayKeys[now.day()] === item.day) {
+      todayHoursStart = item.open;
+      todayHoursEnd = item.close;
+    }
+  });
+
+  // Add the regular hours values to sortable stacks of hours.
+  const startTimes = [todayHoursStart];
+  const endTimes = [todayHoursEnd];
+
+  const hasActiveClosing = alertsOpenStatus;
+
+  if (!hasActiveClosing) {
+    //console.log(hasActiveClosing);
+
+    // We have alerts, so map over them.
+    alerts.map(alert => {
+      // Check if closed_for key exists
+      if ('closed_for' in alert) {
+        // Check for start and end values
+        if (alert.applies.start && alert.applies.end) {
+          // Closing hours
+          const closingStartHours = dayjs(alert.applies.start).format('HH:mm');
+          const closingEndHours = dayjs(alert.applies.end).format('HH:mm');
+
+          if (closingStartHours > todayHoursStart) {
+            endTimes.push(closingStartHours);
+          }
+
+          if (closingEndHours < todayHoursEnd) {
+            startTimes.push(closingEndHours);
+          }
+        }
+      }
+    });
+
+    // Sort the start and end times lowest to highest.
+    //natsort(startTimes);
+    //natsort(endTimes);
+    startTimes.sort();
+    endTimes.sort();
+
+    console.log('sorted start and end times');
+    console.log(startTimes);
+    console.log(endTimes);
+
+    // Use the latest start time and the earliest end time.
+    const startTime = startTimes.slice(-1).pop();
+    const endTime = endTimes[0];
+
+    console.log('latest start time and the earliest end time');
+    console.log(startTime);
+    console.log(endTime);
+
+    if (startTime < endTime) {
+      todayHoursStart = startTime;
+      todayHoursEnd = endTime;
+    }
+  }
+
+  const todaysHours = {
+    start: todayHoursStart,
+    end: todayHoursEnd
+  };
+
+  return todaysHours;
+}
+
+export default setTodaysHours;
---
+++ src/utils/setTodaysHours.test.js
@@ -0,0 +1,94 @@
+import setTodaysHours from './setTodaysHours';
+
+// DayJS
+const dayjs = require('dayjs');
+// DayJS timezone
+var utc = require('dayjs/plugin/utc');
+var timezone = require('dayjs/plugin/timezone');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+describe('setTodaysHours', () => {
+  // Create a dayJS date object for now.
+  let now = dayjs('2020-10-28T12:00:00').tz('America/New_York');
+
+  const regularHours = [
+    {
+      day: 'Wed.',
+      open: '10:00',
+      close: '18:00'
+    }
+  ];
+
+  /**
+   * Late Opening
+   *
+   * today: October 28, 12:00pm
+   * regular hours start: 10:00am
+   * regular hours end: 6:00pm
+   * closing start: October 28, 2020 10:00am
+   * closing end: October 28, 2020, 12:00pm
+   *
+   * Expected: Location opens at 12:00pm, instead of 10:00am. Todays Hour's: 12pm-6pm
+   */
+  const lateOpeningAlerts = [
+    {
+      closed_for: 'Late Opening',
+      applies: {
+        start: '2020-10-28T10:00:00-04:00',
+        end: '2020-10-28T12:00:00-04:00'
+      }
+    }
+  ];
+
+  const lateOpeningExpectedHours = {
+    start: '12:00',
+    end: '18:00'
+  };
+
+  test('Late Opening', () => {
+    const todayHours = setTodaysHours(now, regularHours, lateOpeningAlerts, false);
+    expect(todayHours).toMatchObject(lateOpeningExpectedHours);
+  });
+
+  /**
+   * Early Closing
+   *
+   * today: October 28, 12:00pm
+   * regular hours start: 10:00am
+   * regular hours end: 6:00pm
+   * closing start: October 28, 2020 16:00am
+   * closing end: October 28, 2020, 20:00pm
+   *
+   * Expected: Location closes early at 4:00pm, instead of 6:00pm. Todays Hour's: 10am-4pm
+   */
+  const earlyClosingAlerts = [
+    {
+      closed_for: 'Early Closing.',
+      applies: {
+        start: '2020-10-28T16:00:00-04:00',
+        end: '2020-10-28T20:00:00-04:00'
+      }
+    }
+  ];
+
+  const earlyClosingExpectedHours = {
+    start: '10:00',
+    end: '16:00'
+  };
+
+  test('Early Closing', () => {
+    const todayHours = setTodaysHours(now, regularHours, earlyClosingAlerts, false);
+    expect(todayHours).toMatchObject(earlyClosingExpectedHours);
+  });
+
+
+
+
+  /*test('Future Early Closing', () => {
+    const todayHours = setTodaysHours(now, regularHours, earlyClosingAlerts, false);
+    expect(todayHours).toMatchObject(earlyClosingExpectedHours);
+  });
+  */
+
+});
