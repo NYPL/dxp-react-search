5e7d68bc7e56ca2f98ddf39a2e63438d3caf0170
Matt Misselbeck <matt@misselbeck.com>
2020-12-19 12:10:22 -0500

    RENO-1826: Add tests checking the renderMessage of the SearchResultsDetails component

--- src/components/location-finder/SearchResultsDetails/SearchResultsDetails.test.js
+++ src/components/location-finder/SearchResultsDetails/SearchResultsDetails.test.js
@@ -22,23 +22,70 @@ describe('SearchResultsDetails', () => {
     );
     expect(container).toHaveTextContent('Showing 92 results near 125th Library, by distance and open now. Clear all search terms.');
     // console.log() equiv, will show output of component.
-    screen.debug(container);
+    //screen.debug(container);
   });
   
-  // @TODO: Change state values based on logic in SearchResultsDetails > renderMessage() function.
-  /*test('renders SearchResultsDetails Open now only', () => {
+  // Test state values based on logic in SearchResultsDetails > renderMessage() function.
+  test('renders SearchResultsDetails open now only', () => {
+    const { container } = render(
+      <SearchResultsDetails />, { 
+        initialState: { 
+          search: {
+            searchQuery: '',
+            resultsCount: 50,
+            openNow: true
+          } 
+        } 
+      }
+    );
+    expect(container).toHaveTextContent('Showing 50 results open now. Clear all search terms.');
   });
 
-  test('renders SearchResultsDetails Search query only.', () => {
+  test('renders SearchResultsDetails search query only', () => {
+    const { container } = render(
+      <SearchResultsDetails />, { 
+        initialState: { 
+          search: {
+            searchQuery: 'New York Botanical Garden',
+            resultsCount: 92,
+            openNow: false
+          } 
+        } 
+      }
+    );
+    expect(container).toHaveTextContent('Showing 92 results near New York Botanical Garden, by distance. Clear all search terms.');
   });
 
-  test('renders SearchResultsDetails Search query + open now..', () => {
+  test('renders SearchResultsDetails search query + open now', () => {
+    const { container } = render(
+      <SearchResultsDetails />, { 
+        initialState: { 
+          search: {
+            searchQuery: 'Washington Square Park',
+            resultsCount: 50,
+            openNow: true
+          } 
+        } 
+      }
+    );
+    expect(container).toHaveTextContent('Showing 50 results near Washington Square Park, by distance and open now. Clear all search terms.');
   });
 
-  test('renders SearchResultsDetails No results.', () => {
+  test('renders SearchResultsDetails no results', () => {
+    const { container } = render(
+      <SearchResultsDetails />, { 
+        initialState: { 
+          search: {
+            searchQuery: '',
+            resultsCount: 0,
+            openNow: true
+          } 
+        } 
+      }
+    );
+    expect(container).toHaveTextContent('No results found.');
   });
-  */
-    
+  
   // Accessbiility tests.
   test('should not have basic accessibility issues', async () => {
     const { container } = render(
