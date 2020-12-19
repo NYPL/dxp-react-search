5a633cd09de0b0785b3b12be8e18336a6f0a6cd4
Matt Misselbeck <matt@misselbeck.com>
2020-12-19 13:46:29 -0500

    RENO-1826: Add tests to the SearchHeader component

---
+++ src/components/location-finder/SearchHeader/SearchHeader.test.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { axe, toHaveNoViolations } from 'jest-axe';
+import '@testing-library/jest-dom/extend-expect';
+import { MockedProvider } from '@apollo/client/testing';
+import { render } from './../../../../testHelper/customRtlRender';
+import SearchHeader from './SearchHeader';
+
+expect.extend(toHaveNoViolations);
+
+const mocks = [];
+
+describe('SearchHeader', () => {
+  test('renders SearchHeader component', () => {
+    render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <SearchHeader />
+      </MockedProvider>
+    );
+  });
+
+  test('SearchHeader component should have title', () => {
+    const { container } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <SearchHeader />
+      </MockedProvider>
+    );
+    expect(container.querySelector('#location-finder__title')).toBeInTheDocument();
+    screen.debug(container);
+  });
+
+  test('SearchHeader component should have description', () => {
+    const { container } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <SearchHeader />
+      </MockedProvider>
+    );
+    expect(container.querySelector('.location-finder__description')).toBeInTheDocument();
+  });
+
+  test('SearchHeader component should have a search form', () => {
+    render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <SearchHeader />
+      </MockedProvider>
+    );
+    expect(screen.getByRole('search')).toBeInTheDocument();
+  });
+
+  // Accessbiility tests.
+  // Currently Fails on autocomplete listbox form item
+  // Required ARIA child role not present: option
+  /* test('should not have basic accessibility issues', async () => {
+    const { container } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <SearchHeader />
+      </MockedProvider>
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  }); */
+});
