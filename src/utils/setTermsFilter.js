c0b9ff4c74386038bfbd7c5ecad43cbb92cfbb5f
wluisi <w.luisi@williamluisi.org>
2021-02-22 21:43:49 -0500

    set hardcoded uuids for vocabs

--- src/utils/setTermsFilter.js
+++ src/utils/setTermsFilter.js
@@ -1,9 +1,13 @@
 //
 export const BOROUGHS_UUID = 'filter-borough';
 export const ACCESSIBILITY_UUID = 'filter-accessibility';
-export const AMENITIES_UUID = 'filter-amenities';
+/*export const AMENITIES_UUID = 'filter-amenities';
 export const SUBJECTS_UUID = 'filter-subjects';
 export const MEDIA_UUID = 'filter-media';
+*/
+export const AMENITIES_UUID = 'fa817186-d735-4a05-8b72-388d3b6c7a14';
+export const SUBJECTS_UUID = '9597730e-da47-4a4e-9f2d-5a4fc7b7fd41';
+export const MEDIA_UUID = '4805f571-ec30-4901-912e-e88b41fb158e';
 
 /**
  * Converts a redux state object to the gql format, and adds operator.
@@ -16,7 +20,6 @@ function setTermsFilter(stateObject) {
   // Add the operator
   let operator;
   for (let [key, value] of Object.entries(stateObject)) {
-    console.log(key);
     switch (key) {
       case BOROUGHS_UUID:
       case ACCESSIBILITY_UUID:
