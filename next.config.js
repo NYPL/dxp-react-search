e30aad79de9e0631fad7f7450aa41dd8eb95fb26
nypl-wluisi <62392174+nypl-wluisi@users.noreply.github.com>
2021-03-03 18:43:29 -0500

    Small Changes for Vercel Preview Envs (#100)
    * Adds conditional for ASSET_PREFIX to fix broken builds on Vercel.
    Co-authored-by: wluisi <w.luisi@williamluisi.org>

--- next.config.js
+++ next.config.js
@@ -3,10 +3,16 @@ const { ASSET_PREFIX } = process.env;
 module.exports = (nextConfig = {}) => {
   return Object.assign({}, nextConfig, {
     assetPrefix: ASSET_PREFIX,
-    rewrites() {
-      return [
-        { source: `${ASSET_PREFIX}/_next/:path*`, destination: '/_next/:path*' }
-      ]
+    async rewrites() {
+      if (ASSET_PREFIX) {
+        return [
+          { 
+            source: `${ASSET_PREFIX}/_next/:path*`, 
+            destination: '/_next/:path*' 
+          }
+        ];
+      }
+      return [];
     },
     webpack(config, options) {
       const { dir } = options
