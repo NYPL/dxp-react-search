07c4643b431f48d9a2568f81af1602f923d54064
nypl-wluisi <62392174+nypl-wluisi@users.noreply.github.com>
2021-03-12 17:17:32 -0500

    Filters Use Context for Shared State (#106)
    * working draft of using context for shared state on search filters
    
    * code cleanup
    
    * remove loading skeleton for search filters
    
    Co-authored-by: wluisi <w.luisi@williamluisi.org>

---
+++ src/components/location-finder/SearchFilters/CheckboxList.js
@@ -0,0 +1,77 @@
+import React, { useContext } from 'react';
+// Components
+import { Checkbox, List } from '@nypl/design-system-react-components';
+// Context
+import { SearchFiltersContext } from './SearchFiltersContext';
+
+function CheckboxList(props) {
+  const [state, dispatch] = useContext(SearchFiltersContext);
+  const { vocab } = props;
+  const { checkedTerms } = state;
+
+  function onChangeFilters(vocabId, event) {
+    const selectedItemId = event.target.id;
+    dispatch({
+      type: 'SET_SELECTED_ITEMS',
+      payload: { 
+        selectedItemId: selectedItemId,
+        parentId: vocabId
+      }
+    });
+  }
+
+  function setFilterCheckedProp(vocabId, termId) {
+    let filterChecked = false;
+    if (checkedTerms[vocabId] !== undefined) {
+      filterChecked = checkedTerms[vocabId].terms.find((filter) => filter === termId) 
+    }
+    return filterChecked;
+  }
+
+  return (
+    <List 
+      type='ul' 
+      modifiers={['no-list-styling']}
+    >
+      {vocab.terms.map((term) => {
+        return (
+          <li key={term.id} className="term">
+            <Checkbox
+              checkboxId={term.id}
+              labelOptions={{
+                labelContent: <>{term.name}</>
+              }}
+              name={term.name}
+              checked={setFilterCheckedProp(vocab.id, term.id) || false}
+              onChange={(e) => onChangeFilters(vocab.id, e)}
+            />
+              {term.children &&
+                <List 
+                  type='ul' 
+                  modifiers={['no-list-styling']}
+                >
+                  {term.children.map((childTerm) => {
+                    return (
+                      <li key={childTerm.id} className="term-child">
+                        <Checkbox
+                          checkboxId={childTerm.id}
+                          labelOptions={{
+                            labelContent: <>{childTerm.name}</>
+                          }}
+                          name={childTerm.name}
+                          checked={setFilterCheckedProp(vocab.id, childTerm.id) || false}
+                          onChange={(e) => onChangeFilters(vocab.id, e)}
+                        />
+                      </li>
+                    )
+                  })}
+                </List>
+              }
+          </li>
+        );
+      })}
+    </List>
+  )
+}
+
+export default CheckboxList;
---
+++ src/components/location-finder/SearchFilters/SearchFiltersContext.js
@@ -0,0 +1,192 @@
+import React, { 
+  createContext, 
+  useContext, 
+  useReducer
+} from 'react';
+import isEqual from 'lodash.isequal';
+
+const initialState = {
+  checkedTerms: {}
+};
+
+// Reducer
+function reducer(state, action) {
+  switch (action.type) {
+    case 'SET_SELECTED_DROPDOWNS': {
+      let dropdownIdChecked = action.payload.dropdownIdChecked;
+      const savedItems = action.payload.savedItems;
+      const vocabId = action.payload.vocabId;
+
+      let dropdownIdsCopy;
+      
+      let mode = action.payload.mode;
+
+      const nextState = (object, property) => {
+        let {[property]: omit, ...rest} = object
+        return rest;
+      }
+
+      if (state.dropdownIds !== undefined) {
+        let dropdownIdExists = state.dropdownIds.indexOf(dropdownIdChecked) > -1;
+        // Make a copy of the existing array.
+        dropdownIdsCopy = state.dropdownIds.slice();
+        // If dropdownIdExists exists, remove it from the array.
+        if (dropdownIdExists) {
+          dropdownIdsCopy = dropdownIdsCopy.filter((id) => id != dropdownIdChecked);
+        } else {
+          // Desktop
+          if (mode === 'desktop') {
+            // Desktop: only allow 1 item in the array.
+            dropdownIdsCopy = [dropdownIdChecked];
+          } else {
+            // Mobile: allow multiple items in the array.
+            dropdownIdsCopy.push(dropdownIdChecked);
+          }
+        }
+      } else {
+        // No dropdowns open, so add the checked dropdown to the array.
+        dropdownIdsCopy = [dropdownIdChecked];
+      }
+      
+      // Desktop logic only for clearing/restoring unsaved/saved selectedItems.
+      if (mode === 'desktop') {        
+        // Restore context state to redux state.
+        if (
+          // Check if we have any savedItems.
+          savedItems !== undefined
+          // Check if we have any savedItems for this parent group.
+          && savedItems.hasOwnProperty(vocabId)
+          // Check if the two state objects are not equal.
+          && !isEqual(state.checkedTerms, savedItems)
+        ) {
+          return {
+            ...state,
+            dropdownIds: dropdownIdsCopy,
+            checkedTerms: {
+              ...state.checkedTerms,
+              [vocabId]: {
+                terms: savedItems[vocabId].terms
+              }
+            }
+          };
+        // Parent group has no saved items, so we clear the unsaved context state.
+        } else if (
+          // Check if we have any savedItems.
+          savedItems !== undefined
+          // Check if we have any savedItems for this parent group.
+          && !savedItems.hasOwnProperty(vocabId)
+        ) {
+          return {
+            ...state,
+            dropdownIds: dropdownIdsCopy,
+            checkedTerms: nextState(state.checkedTerms, vocabId)
+          };
+        }
+      }
+
+      // Mobile only
+      return {
+        ...state,
+        dropdownIds: dropdownIdsCopy
+      }
+    }
+    
+    case 'SET_SELECTED_ITEMS': {      
+      const termId = action.payload.selectedItemId;
+      const vocabId = action.payload.parentId;
+
+      const nextState = (object, property) => {
+        let {[property]: omit, ...rest} = object
+        return rest;
+      }
+
+      let termIds;
+      // Check if the tid already exists in the state
+      if (state.checkedTerms[vocabId] !== undefined) {
+        let termIdExists = state.checkedTerms[vocabId].terms.indexOf(termId) > -1;
+        // Make a copy of the existing array.
+        termIds = state.checkedTerms[vocabId].terms.slice();
+        // If termId exists, remove it from the array.
+        if (termIdExists) {
+          termIds = termIds.filter((id) => id != termId);
+        } else {
+          // Add it to the array, but modify the copy, not the original.
+          termIds.push(termId);
+        }
+      } else {
+        termIds = [];
+        termIds.push(termId);
+      }
+  
+      // Check if there are no terms to save, and clear the state if so.
+      if (termIds.length == 0) {
+        return {
+          ...state,
+          checkedTerms: nextState(state.checkedTerms, vocabId)
+        };
+      } else {
+        return {
+          ...state,
+          checkedTerms: {
+            ...state.checkedTerms,
+            [vocabId]: {
+              terms: termIds
+            }
+          }
+        };
+      }
+    }
+
+    case 'RESET_SELECTED_ITEMS_BY_PARENT_ID': {
+      const parentId = action.payload.parentId;
+      
+      // @TODO move to util function?!
+      const nextState = (object, property) => {
+        let {[property]: omit, ...rest} = object
+        return rest;
+      }
+
+      return {
+        ...state,
+        checkedTerms: nextState(state.checkedTerms, parentId),
+        dropdownIds: undefined
+      };
+    }
+
+    case 'RESET_SELECTED_ITEMS': {
+      return {
+        ...state,
+        checkedTerms: {},
+        dropdownIds: undefined
+      };
+    }
+
+    default: {
+      throw new Error(`Unsupported action type: ${action.type}`)
+    }
+  }
+}
+
+const SearchFiltersContext = createContext();
+
+function useSearchFilters() {
+  const context = useContext(SearchFiltersContext);
+
+  if (!context) {
+    throw new Error(`useSearchFilters must be used within a SearchFiltersProvider`)
+  }
+
+  return context;
+}
+
+function SearchFiltersProvider(props) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  
+  return (
+    <SearchFiltersContext.Provider value={[state, dispatch]}>
+      {props.children}
+    </SearchFiltersContext.Provider>
+  );
+}
+
+export { SearchFiltersContext, SearchFiltersProvider, useSearchFilters };
