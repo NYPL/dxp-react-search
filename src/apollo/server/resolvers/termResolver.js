6d860af822659700968f9ce09c43c4cb54fa9851
wluisi <w.luisi@williamluisi.org>
2021-03-19 10:41:22 -0400

    RENO-2106: subject allow list terms

--- src/apollo/server/resolvers/termResolver.js
+++ src/apollo/server/resolvers/termResolver.js
@@ -1,11 +1,25 @@
 // Utils
 import setNestedTerms from './../../../utils/setNestedTerms';
+import getSubjectsAllowList from './../../../utils/getSubjectsAllowList';
+import { SUBJECTS_UUID } from './../../../utils/setTermsFilter';
 
 const termResolver = {
   Query: {
     allTerms: async (parent, args, { dataSources }) => {
       const allTerms = await dataSources.refineryApi.getAllTerms();
-      return allTerms;
+      // Get the terms tree from all Divisions.
+      const subjectsAllowList = getSubjectsAllowList(allTerms.divisions);
+
+      // Replace subject terms with the allow list terms.
+      allTerms.searchFilters.map(filterGroup => {
+        if (filterGroup.uuid === SUBJECTS_UUID) {
+          return filterGroup.terms = subjectsAllowList;
+        } else {
+          return filterGroup.terms
+        }
+      });
+
+      return allTerms.searchFilters;
     },
   },
   Vocab: {
