8a8f26c999d2834515977e872ff10c95fe01b86d
wluisi <w.luisi@williamluisi.org>
2021-04-19 14:32:57 -0400

    fixed failing tests for sortByDistance

--- src/utils/sortByDistance.test.js
+++ src/utils/sortByDistance.test.js
@@ -10,8 +10,8 @@ describe('sortByDistance', () => {
       geolocation: {
         type: "Point",
         coordinates: [
-          -73.9350089,
-          40.8030055
+          -73.9354,
+          40.8034
         ]
       }
     },
@@ -21,8 +21,8 @@ describe('sortByDistance', () => {
       geolocation: {
         type: "Point",
         coordinates: [
-          -73.98170549999999,
-          40.8030055
+          -73.9818,
+          40.752
         ]
       }
     },
@@ -36,10 +36,21 @@ describe('sortByDistance', () => {
           40.7534887
         ]
       }
+    },
+    {
+      name: "DeWitt Wallace Periodical Room",
+      slug: "periodicals-room",
+      geolocation: {
+        type: "Point",
+        coordinates: [
+          -73.9808774,
+          40.7534887
+        ]
+      }
     }
   ];
 
-  test('Search for SASB geocords should return SASB.', () => {
+  test('Search for SASB should return SASB first.', () => {
     // SASB
     const origin = {
       'originLat': 40.7534887,
@@ -50,14 +61,25 @@ describe('sortByDistance', () => {
     expect(result[0].slug).toEqual('sasb');
   });
 
-  test('Search for SNFL geocords should return SNFL.', () => {
+  test('Search for SNFL should return SNFL before SASB.', () => {
     // SNFL
-    const origin2 = {
+    const origin = {
       'originLat': 40.7518863,
       'originLng': -73.98170549999999
     };
 
-    const result = sortByDistance(origin2, locations, 'Stavros Niarchos Foundation');
+    const result = sortByDistance(origin, locations, 'Stavros Niarchos Foundation');
     expect(result[0].slug).toEqual('snfl');
+  });
+
+  test('Search for Dewitt should return Dewitt before SASB.', () => {
+    // Dewitt/SASB
+    const origin = {
+      'originLat': 40.7534887,
+      'originLng': -73.9808774
+    };
+
+    const result = sortByDistance(origin, locations, 'Dewitt');
+    expect(result[0].slug).toEqual('periodicals-room');
   });
 });
