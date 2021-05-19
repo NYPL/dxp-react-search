b4c703db83ed328cd60ea743f155ae476662dbf7
wluisi <w.luisi@williamluisi.org>
2021-05-19 10:18:46 -0400

    small content change for testing

--- src/components/location-finder/SearchHeader/content.js
+++ src/components/location-finder/SearchHeader/content.js
@@ -1,5 +1,5 @@
 const SearchHeaderContent = {
-  'title': 'Find Your Library',
+  'title': 'Find Your Library Tugboat QA Test',
   'description': 'Enter an address or landmark to search nearby or type in a Library name.',
 };
 
