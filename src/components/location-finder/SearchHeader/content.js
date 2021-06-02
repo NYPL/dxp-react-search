08d69c09ab091c3ad1a9e808eee2271aecfc2d84
wluisi <w.luisi@williamluisi.org>
2021-06-01 20:27:38 -0400

    use shared SearchHeader component and update lf and or pages

--- src/components/location-finder/SearchHeader/content.js
+++
@@ -1,6 +0,0 @@
-const SearchHeaderContent = {
-  'title': 'Find Your Library',
-  'description': 'Enter an address or landmark to search nearby or type in a Library name.',
-};
-
-export default SearchHeaderContent;
