467889efec6448e65e4225a64fa7680f850eac07
wluisi <w.luisi@williamluisi.org>
2021-06-03 17:11:06 -0400

    code cleanup for RightRail

--- src/components/shared/RightRail/RightRail.js
+++ src/components/shared/RightRail/RightRail.js
@@ -2,29 +2,23 @@ import React, { Fragment } from 'react';
 // Content
 import RightRailContent from './content';
 // Components
-import * as DS from '@nypl/design-system-react-components';
 import MenuGroup from '../../shared/MenuGroup';
 
 function RightRail() {
-  // Content
-  const { more_at_nypl, need_help, support_nypl } = RightRailContent;
-
   return (
-    <div className="bottom-rails">
-      <Fragment>
-        {RightRailContent.map((menu) => {
-          return (
-            <MenuGroup
-              id={menu.id}
-              headingId={menu.title.toLowerCase().replace(/\s/g, '-')}
-              title={menu.title}
-              items={menu.items}
-              orientation="horizontal"
-            />
-          )
-        })}
-      </Fragment>
-    </div>
+    <Fragment>
+      {RightRailContent.map((menu) => {
+        return (
+          <MenuGroup
+            id={menu.id}
+            headingId={menu.title.toLowerCase().replace(/\s/g, '-')}
+            title={menu.title}
+            items={menu.items}
+            orientation="horizontal"
+          />
+        )
+      })}
+    </Fragment>
   );
 };
 
