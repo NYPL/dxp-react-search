8b8636f7b1a181ae86b1fa3ebecc7efbe4d7ee09
Matt Misselbeck <matt@misselbeck.com>
2021-07-01 14:40:58 -0400

    Verify login page

---
+++ src/pages/research/collections/online-resources/verify.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+// Apollo
+import { withApollo } from '../../../../apollo/client/withApollo';
+// Redux
+import { withRedux } from '../../../../redux/withRedux';
+// Components
+import PageContainer from '../../../../components/online-resources/layouts/PageContainerNoContentHeader';
+import VerifyForm from '../../../../components/online-resources/VerifyForm';
+// Utils
+import { ONLINE_RESOURCES_BASE_PATH } from './../../../../utils/config';
+
+function OnlineResourcesVerifyPage() {
+  return (
+    <PageContainer
+      metaTags={{
+        title: 'Log in to use this database',
+        description: 'Enter library card number to access database',
+        url: `https://www.nypl.org${ONLINE_RESOURCES_BASE_PATH}/verify`
+      }}
+      contentPrimary={
+        <VerifyForm />
+      }
+    />
+  );
+}
+
+export default withApollo(
+  withRedux((OnlineResourcesVerifyPage)), { 
+    ssr: true, 
+    redirects: false 
+  });
