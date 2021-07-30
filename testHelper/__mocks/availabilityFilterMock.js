895a270ff2191a666a162c40f059bea689dfa464
wluisi <w.luisi@williamluisi.org>
2021-07-30 10:21:46 -0400

    added back available without restrictions

--- testHelper/__mocks/availabilityFilterMock.js
+++ testHelper/__mocks/availabilityFilterMock.js
@@ -2,15 +2,14 @@ const availabilityFilterMock = {
   "data": {
     "id": "availability",
     "terms": [
-      /*{
+      {
         "uuid": "9507-25d4b9b29dd0",
         "tid": "no-restrictions",
-        "name": "Available without restrictions",
+        "name": "Available everywhere",
         "vid": "20000000",
         "parent_tid": "virtual",
         "parent_uuid": "virtual"
       },
-      */
       {
         "uuid": "-c760d4f5041e",
         "tid": "card-required",
