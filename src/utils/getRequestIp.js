ce8669f7d424a55ba00c8795dd0ac7ff86b24f71
nypl-wluisi <62392174+nypl-wluisi@users.noreply.github.com>
2021-08-06 22:33:32 -0400

    Different approach for getting ip address (#168)
    * console log the headers to server
    
    * testing alt way of getting ip address
    
    * fix stupid bug
    
    * more testing and code cleanup
    
    * code cleanup
    
    * code cleanup, move ip checking to useEffect to make sure it runs only on client side
    
    Co-authored-by: wluisi <w.luisi@williamluisi.org>

---
+++ src/utils/getRequestIp.js
@@ -0,0 +1,30 @@
+import requestIp from 'request-ip';
+
+/**
+ * Get the request ip address from a variety of sources.
+ *
+ * @param {object} request - request object contains the 
+ * @return {string} ipAddress - the clients up address.
+ */
+function getRequestIp(request, debugIpAddress) {
+  // Default to the request-ip value for the ip address.
+  let ipAddress = requestIp.getClientIp(request);
+  // Check x-forwarded-for header.
+  // This will return multiple values, need to get the first one.
+  // Ex: '200.28.171.103, 10.255.0.25, 10.0.0.59,::ffff:10.0.0.187,::ffff:10.0.0.168'
+  if (request.headers['x-forwarded-for']) {
+    const addresses = request.headers['x-forwarded-for'].split(',');
+    ipAddress = addresses[0];
+  }
+  // Use the Imperva IP if headers are present, with external IP 65.88.x.x
+  if (request.headers['incap-client-ip']) {
+    ipAddress = request.headers['incap-client-ip'];
+  }
+  // Use test_ip value if it's set as a query param for debug mode.
+  if (debugIpAddress) {
+    ipAddress = debugIpAddress;
+  }
+  return ipAddress;
+}
+
+export default getRequestIp;
