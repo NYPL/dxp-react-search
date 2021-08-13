0c57c145f328d2f8e8cb02d312eb25333684bbaf
wluisi <w.luisi@williamluisi.org>
2021-08-12 21:02:43 -0400

    testing husky on commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import type { AppProps } from 'next/app';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import type { AppProps } from "next/app";
 const { NEXT_PUBLIC_GA_TRACKING_ID } = process.env;
-import './../styles/main.scss';
-import AppLayout from './../components/shared/layouts/AppLayout';
+import "./../styles/main.scss";
+import AppLayout from "./../components/shared/layouts/AppLayout";
 
 function SearchApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -11,21 +11,19 @@ function SearchApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     const handleRouteChange = (url) => {
       // @ts-ignore
-      window.gtag('config', NEXT_PUBLIC_GA_TRACKING_ID, {
+      window.gtag("config", NEXT_PUBLIC_GA_TRACKING_ID, {
         page_path: url,
       });
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
   }, [router.events]);
 
   return (
     <AppLayout>
       <Component {...pageProps} />
-
-      
     </AppLayout>
   );
 }
