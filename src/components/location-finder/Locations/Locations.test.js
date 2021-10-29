716f305dad3dc0f85727d3586506e302b4ca131b
wluisi <w.luisi@williamluisi.org>
2021-10-29 14:11:06 -0400

    fix for lf loading skeleton

--- src/components/location-finder/Locations/Locations.test.js
+++ src/components/location-finder/Locations/Locations.test.js
@@ -54,7 +54,7 @@ describe("Apollo states test", () => {
     );
 
     // Check for loading skeleton class
-    expect(container.getElementsByClassName("skeleton-loader").length).toBe(1);
+    expect(container.getElementsByClassName("skeleton-loader").length).toBe(3);
   });
 
   // Error state
