e9c659814cdca2b0a35b564282dac5a03bf1a7f8
wluisi <w.luisi@williamluisi.org>
2022-01-13 19:55:41 -0500

    code cleanup

--- src/apollo/server/resolvers/decoupledRouterResolver.js
+++ src/apollo/server/resolvers/decoupledRouterResolver.js
@@ -8,17 +8,18 @@ const decoupledRouterResolver = {
   DecoupledRouter: {
     id: (parent, args, context, info) => {
       const router = parent;
+      const queryArgs = info.variableValues;
       // Set a default for id to the path requested in query, since its non nullable.
-      let id = info.variableValues.path;
+      let id = queryArgs.path;
       // Check if entity is returned by router, which would include the uuid.
       if (router.entity) {
-        id = `router-id-${router.entity.uuid}`;
+        id = router.entity.uuid;
       }
       // This is not a drupal route, so return the resolved redirect as id.
       if (!router.entity && router.resolved) {
-        id = `router-id-${router.resolved}`;
+        id = router.resolved;
       }
-      return id;
+      return `router-id-${id}`;
     },
     uuid: (router) => {
       return router.entity?.uuid ? router.entity.uuid : null;
