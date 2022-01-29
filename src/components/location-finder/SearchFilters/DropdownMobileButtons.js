06da405e0eb85ed9e9a4b20bdf1e4745373e6a66
wluisi <w.luisi@williamluisi.org>
2022-01-29 12:30:32 -0500

    fix for legacy lf filterbar top buttons

--- src/components/location-finder/SearchFilters/DropdownMobileButtons.js
+++ src/components/location-finder/SearchFilters/DropdownMobileButtons.js
@@ -1,14 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 // Context
-import { SearchFiltersContext } from './SearchFiltersContext';
+import { SearchFiltersContext } from "./SearchFiltersContext";
 // Redux
-import { useDispatch } from 'react-redux';
-import { setFilters } from '../../../redux/actions';
+import { useDispatch } from "react-redux";
+import { setFilters } from "../../../redux/actions";
 // Components
-import { 
+import {
   Button,
-  Icon
-} from '@nypl/design-system-react-components';
+  ButtonTypes,
+  Icon,
+  IconAlign,
+  IconNames,
+  IconRotationTypes,
+  IconSizes,
+} from "@nypl/design-system-react-components";
 
 function DropdownMobileButtons(props) {
   // Context state.
@@ -19,7 +24,7 @@ function DropdownMobileButtons(props) {
   // Redux
   const reduxDispatch = useDispatch();
 
-  function onClearAllFilters(event) {    
+  function onClearAllFilters(event) {
     // Close modal
     setIsModalOpen(false);
   }
@@ -27,45 +32,45 @@ function DropdownMobileButtons(props) {
   function onSaveAllFilters(event) {
     // Save the local state into redux.
     // @TODO this works without the searchFilters spread, but are we mutating?
-    reduxDispatch(setFilters({
-      searchFilters: {
-        //...searchFilters,
-        ...checkedTerms
-      }
-    }));
-    
+    reduxDispatch(
+      setFilters({
+        searchFilters: {
+          //...searchFilters,
+          ...checkedTerms,
+        },
+      })
+    );
+
     // Close modal
     setIsModalOpen(false);
   }
 
   return (
-    <div 
-      className="dropdown__content-buttons"
-    >
+    <div className="dropdown__content-buttons">
       <Button
-        buttonType="link"
-        id={'button-clear-all'}
+        buttonType={ButtonTypes.Link}
+        id={"button-clear-all"}
         mouseDown={false}
-        type="button"
         onClick={(e) => onClearAllFilters(e)}
+        additionalStyles={{
+          display: "block",
+          width: "fit-content",
+        }}
       >
         <Icon
-          decorative
-          iconRotation="rotate-90"
-          modifiers={[
-            'small'
-          ]}
-          name="arrow"
+          name={IconNames.Arrow}
+          align={IconAlign.Left}
+          iconRotation={IconRotationTypes.Rotate90}
+          size={IconSizes.Small}
         />
         Go Back
       </Button>
       <Button
-        buttonType="filled"
-        id={'button-save-all'}
+        buttonType={ButtonTypes.Primary}
+        id={"button-save-all"}
         mouseDown={false}
-        type="button"
         onClick={(e) => {
-          onSaveAllFilters(e)
+          onSaveAllFilters(e);
         }}
       >
         Show Results
