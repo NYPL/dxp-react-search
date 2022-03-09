db4266a7a50f85aaeefb33f84603b48ae3001ae8
William Luisi <williamluisi@nypl.org>
2022-03-09 12:06:09 -0500

    cleanup for tests

--- cypress/tests/blogs/blogs-all.test.js
+++ cypress/tests/blogs/blogs-all.test.js
@@ -1,8 +1,3 @@
-/* eslint-disable */
-// Disable ESLint to prevent failing linting inside the Next.js repo.
-// If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
-// https://github.com/cypress-io/eslint-plugin-cypress
-
 describe("Blog All pg tests", () => {
   beforeEach(() => {
     cy.viewport(1024, 768);
@@ -22,7 +17,7 @@ describe("Blog All pg tests", () => {
 
   it("Filter bar should reset pagination when applied", () => {
     cy.log("Goto blogs all page starting at page 15");
-    cy.visit("http://localhost:3000/blog/all?page=15");
+    cy.visit("/blog/all?page=15");
 
     const items = ["Black Culture", "Book Lists"];
     const queryParams = "channel=730+734";
@@ -55,7 +50,7 @@ describe("Blog All pg tests", () => {
   });
 
   it("10 blog posts should display on first page", () => {
-    cy.visit("http://localhost:3000/blog/all");
+    cy.visit("/blog/all");
     cy.log("Get blog collection by test id");
     cy.get("[data-testid=blog-collection]")
       .find("li")
@@ -65,6 +60,7 @@ describe("Blog All pg tests", () => {
   it("Clicking next link in pagination takes user to page 2", () => {});
 
   it("Clicking page 3 link in pagination takes user to page 3", () => {
+    cy.visit("/blog/all");
     cy.log("Find the pagination component");
     cy.findByRole("navigation", {
       name: /pagination/i,
