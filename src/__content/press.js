4a3b2ffb1c4e8920e0d48919d5e774423fe1b1f3
Isa Stettler <stettlerisabelle@gmail.com>
2022-03-11 15:00:00 -0500

    add figma text content and sort for query

--- src/__content/press.js
+++ src/__content/press.js
@@ -1,10 +1,19 @@
 const pressContent = {
+  meta: {
+    title: "Press Releases",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh felis massa augue in egestas a quis adipiscing. Mauris eu purus, orci libero tortor condimentum duis nisl nunc. Vel felis condimentum dolor pretium nibh egestas nulla molestie. Purus elementum tincidunt suspendisse risus.",
+  },
   about: {
     title: "About the New York Public Library",
     description:
       "For 125 years, The New York Public Library has been a free provider of education and information for the people of New York and beyond. With 92 locations—including research and branch libraries—throughout the Bronx, Manhattan, and Staten Island, the Library offers free materials, computer access, classes, exhibitions, programming and more to everyone from toddlers to scholars, and has seen record numbers of attendance and circulation in recent years. The New York Public Library receives approximately 16 million visits through its doors annually and millions more around the globe who use its resources at www.nypl.org. To offer this wide array of free programming, The New York Public Library relies on both public and private funding. Learn more about how to support the Library at ",
     url: "https://www.nypl.org/support",
     anchorText: "nypl.org/support.",
+  },
+  mediaContacts: {
+    bodyText:
+      "For media inquiries about The New York Public Library, contact Senior Publicist Sara Beth Joren at sarabethjoren@nypl.org.\n\nFor media inquiries about The Schomburg Center for Research in Black Culture, contact Assistant Director of Media Relations Amy Geduldig at amygeduldig@nypl.org.\n\nFor media inquiries about The Library for the Performing Arts, contact Assistant Director of Communications for LPA Nora Lyons at noralyons@nypl.org. ",
   },
 };
 
