54b2124e9c622168c4db140857e372f2d05cea53
Isa Stettler <stettlerisabelle@gmail.com>
2022-03-21 13:55:42 -0400

    clean up of TestBottomPromo

--- src/components/location-finder/BottomPromo/TestBottomPromo.tsx
+++ src/components/location-finder/BottomPromo/TestBottomPromo.tsx
@@ -5,8 +5,6 @@ import BottomPromoContent from "../../../__content/locationFinder";
 import {
   Heading,
   HeadingLevels,
-  Icon,
-  IconSizes,
   Image,
   Link,
   LinkTypes,
@@ -17,7 +15,6 @@ import {
   Box,
   Text,
   TextDisplaySizes,
-  ImageSizes,
 } from "@nypl/design-system-react-components";
 import image from "next/image";
 
@@ -25,67 +22,86 @@ function TestBottomPromo() {
   const { promo_left, promo_right } = BottomPromoContent;
 
   return (
-    <Flex
-      direction={["column", "column", "row"]}
-      justify={"center"}
+    <Box
       mx={"calc(-50vw + 50%)"}
       mb="l"
       bgGradient={"linear(to-r,#FFFBF8,#f5f5f5 )"}
     >
-      <Box bg={"#FFFBF8"} w={["65%", "65%", "40%"]} py={"1rem"} pr={"1rem"}>
-        <Heading
-          id="promo-left-section-title"
-          level={HeadingLevels.Two}
-          text={promo_left.title}
-        />
-        <Flex direction={["column", "column", "row"]} justify={"space-between"}>
-          {promo_left.image.map((value, index) => {
-            return (
-              <Box
-                key={value.link}
-                mr={[`${index === image.length - 1 ? "20%" : ""}`, "0"]}
-              >
-                <Link href={value.link} className="promo-link">
-                  <Image alt="" imageCaption={value.name} src={value.url} />
-                </Link>
-              </Box>
-            );
-          })}
-        </Flex>
-      </Box>
-      <Box bg="#f5f5f5" w={["65%", "65%", "40%"]} py={"1rem"} pl={"1rem"}>
-        <Heading
-          id="promo-right-section-title"
-          level={HeadingLevels.Two}
-          text={promo_right.title}
-        />
-        <Text displaySize={TextDisplaySizes.Caption}>
-          {promo_right.description}
-        </Text>
-        <Flex direction="row" justify={["space-between", "flex-start"]}>
-          {promo_right.image.map((value, index) => {
-            const typedLogoName = value.logo as LogoNames;
-            return (
-              <Box key={value.link}>
-                <Link type={LinkTypes.Default}>
-                  <a
-                    aria-label={value.logo_alt}
+      <Flex
+        direction={["column", null, "row"]}
+        justify={[null, null, null, "center"]}
+      >
+        <Box
+          bg="#FFFBF8"
+          w={["100%", null, "50%", null, "624px"]}
+          p={"1rem"}
+          pl={["1rem", null, null, null, "0px"]}
+        >
+          <Heading
+            id="promo-left-section-title"
+            level={HeadingLevels.Two}
+            text={promo_left.title}
+          />
+          <Flex direction={["column", null, "row"]}>
+            {promo_left.image.map((value, index) => {
+              return (
+                <Box
+                  key={value.link}
+                  mr={[
+                    "20%",
+                    null,
+                    `${index === image.length - 1 ? null : "0px"}`,
+                  ]}
+                  mb={["2rem", null, "0px"]}
+                  w={["65%", null, "100%"]}
+                >
+                  <Link
                     href={value.link}
-                    className="promo-link"
+                    textDecoration={"none"}
+                    color={"ui.black"}
                   >
-                    <Logo
-                      // @ts-ignore
-                      name={LogoNames[typedLogoName]}
-                      size={LogoSizes.Small}
-                    />
-                  </a>
-                </Link>
-              </Box>
-            );
-          })}
-        </Flex>
-      </Box>
-    </Flex>
+                    <Image alt="" imageCaption={value.name} src={value.url} />
+                  </Link>
+                </Box>
+              );
+            })}
+          </Flex>
+        </Box>
+        <Box
+          bg="ui.gray.x-light-cool"
+          w={["100%", null, "50%", null, "624px"]}
+          p={"1rem"}
+          ml={["calc(-50vw + 50%)", null, "0px"]}
+        >
+          <Heading
+            id="promo-right-section-title"
+            level={HeadingLevels.Two}
+            text={promo_right.title}
+          />
+          <Text displaySize={TextDisplaySizes.Caption}>
+            {promo_right.description}
+          </Text>
+          <Flex justify={[null, null, "space-between"]}>
+            {promo_right.image.map((value, index) => {
+              const typedLogoName = value.logo as LogoNames;
+              return (
+                <Box key={value.link} w={["50%", null, "40%"]}>
+                  <Link type={LinkTypes.Default}>
+                    <a aria-label={value.logo_alt} href={value.link}>
+                      <Logo
+                        // @ts-ignore
+                        name={LogoNames[typedLogoName]}
+                        size={LogoSizes.Small}
+                      />
+                    </a>
+                  </Link>
+                </Box>
+              );
+            })}
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
   );
 }
 
