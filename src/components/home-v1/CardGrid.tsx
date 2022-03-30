53f69fe0b4c237f033b7b156f9756e27b535a3a4
Isa Stettler <stettlerisabelle@gmail.com>
2022-03-30 15:06:03 -0400

    update CardGrid with props for ComponentWrapper & mobile view

--- src/components/home-v1/CardGrid.tsx
+++ src/components/home-v1/CardGrid.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Heading } from "@chakra-ui/react";
+import { Grid, Heading, Box } from "@chakra-ui/react";
 import ComponentWrapper from "./ComponentWrapper";
 
 interface CardGridProps {
@@ -16,25 +16,40 @@ interface CardGridItem {
 
 function CardGrid({ title, items }: CardGridProps) {
   return (
-    <ComponentWrapper title={title}>
+    <ComponentWrapper
+      title={title}
+      textColor="red.200"
+      borderColor="red"
+      paddingTop={true}
+    >
       <Grid
         as="ul"
         listStyleType="none"
-        templateColumns="repeat(auto-fit, minmax(267px, 1fr))"
+        templateColumns={{
+          base: "1fr",
+          md: "repeat(auto-fit, minmax(267px, 1fr))",
+        }}
+        w="full"
         gap={6}
       >
         {items.map((item: CardGridItem) => (
           <li key={item.id}>
-            <img width="100%" height="auto" src={item.image} />
-            <Heading
-              as="h3"
-              size="md"
-              fontWeight="bold"
-              fontFamily="Kievit-Medium"
-              my={2}
-            >
-              {item.title}
-            </Heading>
+            <Box display={"flex"} flexDirection={{ base: "row", md: "column" }}>
+              <Box w={{ base: "30vw", md: "100%" }}>
+                <img width="100%" height="auto" src={item.image} />
+              </Box>
+              <Heading
+                as="h3"
+                size="md"
+                w={{ base: "60vw", md: "auto" }}
+                fontWeight="bold"
+                fontFamily="Kievit-Book"
+                my={2}
+                mx={{ base: "5vw", md: "0px" }}
+              >
+                {item.title}
+              </Heading>
+            </Box>
           </li>
         ))}
       </Grid>
