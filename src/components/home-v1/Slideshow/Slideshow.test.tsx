2a807628a643d3c6389677ecd54792cfe3e0c5c4
Isa Stettler <stettlerisabelle@gmail.com>
2022-05-10 14:50:31 -0400

    add tests and snapshots for SlideshowButton

--- src/components/home-v1/Slideshow/Slideshow.test.tsx
+++ src/components/home-v1/Slideshow/Slideshow.test.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { axe, toHaveNoViolations } from "jest-axe";
 import "@testing-library/jest-dom/extend-expect";
 expect.extend(toHaveNoViolations);
@@ -12,6 +13,9 @@ import SlideshowButton from "./SlideshowButton";
 const item = {
   id: "test-id-1",
   title: "Test",
+  author: "writer 1",
+  genre: "genre",
+  audience: "people",
   image: "https://placeimg.com/400/200/arch",
   link: "https://www.nypl.org/",
   location: "Stephen A. Schwarzman Building",
@@ -20,44 +24,95 @@ const items = [
   {
     id: "test-id-1",
     title: "Test",
+    author: "writer 1",
+    genre: "genre",
+    audience: "people",
     image: "https://placeimg.com/400/200/arch",
-    link: "https://www.nypl.org/",
+    url: "https://www.nypl.org/",
   },
   {
     id: "test-id-2",
     title: "Test",
+    author: "writer 2",
+    genre: "genre",
+    audience: "people",
     image: "https://placeimg.com/400/200/arch",
-    link: "https://www.nypl.org/",
+    url: "https://www.nypl.org/",
   },
   {
     id: "test-id-3",
     title: "Test",
+    author: "writer 3",
+    genre: "genre",
+    audience: "people",
     image: "https://placeimg.com/400/200/arch",
-    link: "https://www.nypl.org/",
+    url: "https://www.nypl.org/",
   },
   {
     id: "test-id-4",
     title: "Test",
+    author: "writer 4",
+    genre: "genre",
+    audience: "people",
     image: "https://placeimg.com/400/200/arch",
-    link: "https://www.nypl.org/",
+    url: "https://www.nypl.org/",
   },
 ];
 
-describe("SlideshowButton tests", () => {
-  it("shold pass axe accessibility test", () => {});
-  it("should have the correct ");
-  it("should call useSlideshow when clicked", () => {});
-  it("should render the UI snapshot correctly", () => {});
+xdescribe("SlideshowButton tests", () => {
+  let prevSlide: jest.MockedFunction<() => void>;
+  let nextSlide: jest.MockedFunction<() => void>;
+
+  beforeEach(() => {
+    prevSlide = jest.fn();
+    nextSlide = jest.fn();
+  });
+  it("should pass axe accessibility test", async () => {
+    const { container } = render(<SlideshowButton direction="next" />);
+  });
+  it("should render correct button according to direction prop", () => {
+    render(<SlideshowButton direction="next" />);
+    expect(screen.getByRole("button", { name: />/i })).toBeInTheDocument();
+  });
+  it("should call correct function when clicked", () => {
+    render(
+      <SlideshowButton
+        direction="next"
+        nextSlide={nextSlide}
+        prevSlide={prevSlide}
+      />
+    );
+    userEvent.click(screen.getByRole("button", { name: />/i }));
+    expect(nextSlide).toBeCalledTimes(1);
+    expect(prevSlide).not.toBeCalled();
+  });
+  it("should render the UI snapshot correctly", () => {
+    const nextButton = renderer
+      .create(<SlideshowButton direction="next" nextSlide={nextSlide} />)
+      .toJSON();
+    const prevButton = renderer
+      .create(<SlideshowButton direction="prev" prevSlide={prevSlide} />)
+      .toJSON();
+    expect(nextButton).toMatchSnapshot();
+    expect(prevButton).toMatchSnapshot();
+  });
 });
 
-describe("SlideshowContainer tests", () => {
-  it("shold pass axe accessibility test", () => {});
+xdescribe("SlideshowContainer tests", () => {
+  //   it("shold pass axe accessibility test", async () => {
+  //     const { container } = render(<SlideshowContainer items={items} />);
+  //   });
   it("shold render list of cards ", () => {});
   it("should render the UI snapshot correctly", () => {});
 });
 
-describe("Slideshow tests", () => {
-  it("shold pass axe accessibility test", () => {});
+xdescribe("Slideshow tests", () => {
+  it("shold pass axe accessibility test", async () => {
+    const { container } = render(
+      <Slideshow title="Test" link="https://nypl.com" items={items} />
+    );
+    expect(await axe(container)).toHaveNoViolations();
+  });
   it("shold not show the prev button upon rendering", () => {});
   it("shold show the prev button ones the next button has been clicked", () => {});
   it("shold move the SlideshowContainer to the left if the next button is clicked", () => {});
