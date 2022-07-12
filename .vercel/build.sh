702d1d0766ed5bbd9bfeb877b4a5db908c7e1597
William Luisi <williamluisi@nypl.org>
2022-07-11 22:40:01 -0400

    add NEXT_PUBLIC_NYPL_DOMAIN to vercel build script

--- .vercel/build.sh
+++ .vercel/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Set vercel only env variable
+# We set the env vars here and not via Vercel GUI, because we need to use "system env vars"
 export NEXT_PUBLIC_GRAPHQL_API=https://$NEXT_PUBLIC_VERCEL_URL/api/graphql
 echo "NEXT_PUBLIC_GRAPHQL_API: $NEXT_PUBLIC_GRAPHQL_API"
+
+export NEXT_PUBLIC_NYPL_DOMAIN=https://$NEXT_PUBLIC_VERCEL_URL
+echo "NEXT_PUBLIC_NYPL_DOMAIN: $NEXT_PUBLIC_NYPL_DOMAIN"
 
 # Run build
 npm run build
