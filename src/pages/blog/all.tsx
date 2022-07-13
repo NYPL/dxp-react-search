9efb047459415abdb714260a7fe20c772822873b
William Luisi <williamluisi@nypl.org>
2022-07-12 23:46:15 -0400

    add getServerSideProps to blogs all pg

--- src/pages/blog/all.tsx
+++ src/pages/blog/all.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 // Apollo
-import { withApollo } from "./../../apollo/client/withApollo";
+import withApollo from "./../../apollo/withApollo";
+import { initializeApollo } from "./../../apollo/withApollo/apollo";
 // Components
 import PageContainer from "./../../components/blogs/layouts/PageContainer";
-import BlogCollection from "../../components/blogs/BlogCollection";
+import BlogCollection, {
+  BLOGS_QUERY,
+} from "../../components/blogs/BlogCollection/BlogCollection";
 // Content
 import blogsContent from "../../__content/blogs";
 
 function BlogsAllPage() {
   const { meta } = blogsContent;
+
   return (
     <PageContainer
       metaTags={{
@@ -36,7 +40,26 @@ function BlogsAllPage() {
   );
 }
 
-export default withApollo(BlogsAllPage, {
-  ssr: true,
-  redirects: false,
-});
+export const getServerSideProps = async () => {
+  const apolloClient = initializeApollo();
+
+  // @TODO Add Blog filters queries here.
+
+  await apolloClient.query({
+    query: BLOGS_QUERY,
+    variables: {
+      limit: 10,
+      pageNumber: 1,
+      sort: { field: "created", direction: "DESC" },
+      filter: { status: { fieldName: "status", operator: "=", value: true } },
+    },
+  });
+
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract(),
+    },
+  };
+};
+
+export default withApollo(BlogsAllPage);
