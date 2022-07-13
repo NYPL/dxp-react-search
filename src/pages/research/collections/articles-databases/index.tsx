ca76ab6b6a2246a9c8aea5ed6560a51a695dfb43
William Luisi <williamluisi@nypl.org>
2022-07-13 17:10:12 -0400

    update online resources index pg with getStaticProps

--- src/pages/research/collections/articles-databases/index.tsx
+++ src/pages/research/collections/articles-databases/index.tsx
@@ -1,17 +1,23 @@
 import React from "react";
+import { GetStaticProps } from "next";
 // Apollo
-import { withApollo } from "./../../../../apollo/client/withApollo";
+import withApollo from "./../../../../apollo/withApollo";
+import { initializeApollo } from "./../../../../apollo/withApollo/apollo";
 // Redux
 import { withRedux } from "./../../../../redux/withRedux";
 // Components
 import PageContainer from "./../../../../components/online-resources/layouts/PageContainer";
 import AlphabetNav from "./../../../../components/online-resources/AlphabetNav";
-import ResourceTopics from "./../../../../components/online-resources/ResourceTopics";
-import MostPopularResources from "./../../../../components/online-resources/MostPopularResources";
+import ResourceTopics, {
+  RESOURCE_TOPICS_QUERY,
+} from "./../../../../components/online-resources/ResourceTopics/ResourceTopics";
+import MostPopularResources, {
+  MOST_POPULAR_RESOURCES_QUERY,
+} from "./../../../../components/online-resources/MostPopularResources/MostPopularResources";
 // Utils
 import onlineResourcesContent from "./../../../../__content/onlineResources";
 
-function OnlineResources() {
+function OnlineResourcesPage() {
   const { title, description } = onlineResourcesContent;
 
   return (
@@ -44,7 +50,43 @@ function OnlineResources() {
   );
 }
 
-export default withApollo(withRedux(OnlineResources), {
-  ssr: true,
-  redirects: false,
-});
+export const getStaticProps: GetStaticProps = async () => {
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query({
+    query: RESOURCE_TOPICS_QUERY,
+    variables: {
+      filter: {},
+      limit: 30,
+      sort: { field: "weight", direction: "ASC" },
+      vocabulary: "resource_topic",
+    },
+  });
+
+  await apolloClient.query({
+    query: MOST_POPULAR_RESOURCES_QUERY,
+    variables: {
+      filter: {
+        mostPopular: {
+          fieldName: "field_is_most_popular",
+          operator: "IS NOT NULL",
+          value: null,
+        },
+      },
+      limit: 3,
+      pageNumber: 1,
+      sort: { field: "field_is_most_popular", direction: "ASC" },
+    },
+  });
+
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract(),
+    },
+    // 10 mins.
+    revalidate: 600,
+  };
+};
+
+// @ts-ignore
+export default withApollo(withRedux(OnlineResourcesPage));
