c3a13ab2ccfa7876e1d0b2fe7ec47d4830f5fedf
William Luisi <williamluisi@nypl.org>
2022-07-14 21:01:51 -0400

    code cleanup

--- .vercel/build.sh
+++ .vercel/build.sh
@@ -4,9 +4,6 @@
 # We set the env vars here and not via Vercel GUI, because we need these
 # to be dynamic, based on the current vercel preview url.
 
-# This is the clean url for the preview enviornment, that won't change for each deployment. 
-#NEXT_PUBLIC_VERCEL_DOMAIN=scout-git-$NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF-nypl.vercel.app
-
 export NEXT_PUBLIC_GRAPHQL_API=https://$NEXT_PUBLIC_VERCEL_URL/api/graphql
 echo "NEXT_PUBLIC_GRAPHQL_API: $NEXT_PUBLIC_GRAPHQL_API"
 
