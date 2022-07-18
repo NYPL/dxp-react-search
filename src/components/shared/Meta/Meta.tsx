d0573877007489c45b12046d1636b0a0d2076437
William Luisi <williamluisi@nypl.org>
2022-07-18 11:50:34 -0400

    add check in meta for description field before replace function

--- src/components/shared/Meta/Meta.tsx
+++ src/components/shared/Meta/Meta.tsx
@@ -15,7 +15,7 @@ function Meta({ title, description, imageUrl }: MetaProps) {
   const pageUrl = `${NEXT_PUBLIC_NYPL_DOMAIN}${asPath}`;
 
   // Description clean up.
-  const metaDescription = description.replace(/(<([^>]+)>)/gi, "").trim();
+  const metaDescription = description?.replace(/(<([^>]+)>)/gi, "").trim();
 
   // Image default.
   let image = "https://www.nypl.org/sites/default/files/SASBopeng.jpg";
