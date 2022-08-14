ee372dc3ccf723e92b382a623e74a3b077f488e4
William Luisi <williamluisi@nypl.org>
2022-08-14 14:17:03 -0400

    update a+d slug pg for gssp

--- src/pages/research/collections/articles-databases/[slug].tsx
+++ src/pages/research/collections/articles-databases/[slug].tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import * as React from "react";
 // Next
+import { GetServerSidePropsContext } from "next";
 import { useRouter } from "next/router";
 // Apollo
 import { gql, useQuery, useApolloClient } from "@apollo/client";
-import { withApollo } from "./../../../../apollo/client/withApollo";
+import withApollo from "./../../../../apollo/withApollo";
+import { initializeApollo } from "./../../../../apollo/withApollo/apollo";
 import { LOCATION_MATCHES_BY_IP_QUERY } from "./../../../../components/online-resources/SearchResults/SearchResults";
 // Redux
 import { withRedux } from "./../../../../redux/withRedux";
@@ -14,10 +16,13 @@ import { SearchResultsCardSkeletonLoader } from "../../../../components/online-r
 import Error from "./../../../_error";
 // Utils
 import { ONLINE_RESOURCES_BASE_PATH } from "./../../../../utils/config";
-const { NEXT_PUBLIC_NYPL_DOMAIN } = process.env;
 import onlineResourcesContent from "./../../../../__content/onlineResources";
+const { NEXT_PUBLIC_NYPL_DOMAIN } = process.env;
+const { NEXT_PUBLIC_DRUPAL_PREVIEW_SECRET } = process.env;
 // Hooks
-import useDecoupledRouter from "./../../../../hooks/useDecoupledRouter";
+import useDecoupledRouter, {
+  DECOUPLED_ROUTER_QUERY,
+} from "./../../../../hooks/useDecoupledRouter";
 
 export const ONLINE_RESOURCE_BY_ID_QUERY = gql`
   query OnlineResourceByIdQuery($id: String) {
@@ -55,11 +60,11 @@ function OnlineResourceSlug() {
   // Apollo.
   const client = useApolloClient();
   // Local state.
-  const [ipInfo, setIpInfo] = useState();
-  const [clientIpAddress, setClientIpAddress] = useState();
+  const [ipInfo, setIpInfo] = React.useState();
+  const [clientIpAddress, setClientIpAddress] = React.useState();
   // Run a client query after page renders to ensure that the ip address
   // checking is not ssr, but from the client.
-  useEffect(() => {
+  React.useEffect(() => {
     client
       .query({
         query: LOCATION_MATCHES_BY_IP_QUERY,
@@ -151,7 +156,67 @@ function OnlineResourceSlug() {
   );
 }
 
-export default withApollo(withRedux(OnlineResourceSlug), {
-  ssr: true,
-  redirects: true,
-});
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const apolloClient = initializeApollo();
+
+  let uuid;
+
+  // Preview mode.
+  const isPreview =
+    context.query.preview_secret === NEXT_PUBLIC_DRUPAL_PREVIEW_SECRET &&
+    context.query.uuid
+      ? true
+      : false;
+  // Set the uuid for preview mode.
+  if (isPreview) {
+    uuid = context.query.uuid;
+  } else {
+    // Get the slug from the context, i.e., "/blog/whatever-title".
+    const slug = context.resolvedUrl;
+    // Get decoupled router data.
+    const decoupledRouterData = await apolloClient.query({
+      query: DECOUPLED_ROUTER_QUERY,
+      variables: {
+        path: slug,
+      },
+    });
+
+    uuid = await decoupledRouterData?.data?.decoupledRouter?.uuid;
+    const redirect = await decoupledRouterData?.data?.decoupledRouter?.redirect;
+
+    // Redirect logic.
+    // Route is not found in CMS, so set 404 status.
+    if (uuid === null && !redirect) {
+      return {
+        notFound: true,
+      };
+    }
+
+    // Handle the redirect.
+    if (redirect) {
+      return {
+        redirect: {
+          statusCode: 301,
+          destination: redirect.to,
+        },
+        props: {},
+      };
+    }
+  }
+
+  await apolloClient.query({
+    query: ONLINE_RESOURCE_BY_ID_QUERY,
+    variables: {
+      id: uuid,
+    },
+  });
+
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract(),
+    },
+  };
+}
+
+// @ts-ignore
+export default withApollo(withRedux(OnlineResourceSlug));
