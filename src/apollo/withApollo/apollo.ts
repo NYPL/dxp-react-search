9837ab677cdedf9f8c0bb9d7ff631d003b87f37c
William Luisi <williamluisi@nypl.org>
2022-08-16 21:36:43 -0400

    code cleanup for apollo

--- src/apollo/withApollo/apollo.ts
+++ src/apollo/withApollo/apollo.ts
@@ -1,17 +1,21 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from "@apollo/client";
 import { useMemo } from "react";
 import getDataSources from "./../server/datasources/getDataSources";
 import { schema } from "./../server/schema";
 const { NEXT_PUBLIC_GRAPHQL_API } = process.env;
 
-let apolloClient: ApolloClient<any>;
+let apolloClient: ApolloClient<NormalizedCacheObject> | undefined;
 
 function createIsomorphLink() {
-  if (typeof window === "undefined") {
-    // Server
+  const isServer = typeof window === "undefined";
+
+  if (isServer) {
     // SchemaLink allows us to query gql without network calls.
     const { SchemaLink } = require("@apollo/client/link/schema");
-    //const { schema } = require("./../server/schema");
 
     return new SchemaLink({
       schema: schema,
@@ -20,7 +24,6 @@ function createIsomorphLink() {
       },
     });
   } else {
-    // Client
     const { HttpLink } = require("@apollo/client/link/http");
 
     return new HttpLink({
@@ -31,30 +34,46 @@ function createIsomorphLink() {
 }
 
 export function createApolloClient() {
+  const isServer = typeof window === "undefined";
+
   return new ApolloClient({
-    ssrMode: typeof window === "undefined",
+    ssrMode: isServer,
     link: createIsomorphLink(),
     // @TODO add apollo cache overrides from old version.
     cache: new InMemoryCache(),
   });
 }
 
-export function initializeApollo(initialState = null) {
-  // @TODO re-write this whole function, its incomprehensible.
+export function initializeApollo(
+  initialState: NormalizedCacheObject | null = null
+) {
+  const isServer = typeof window === "undefined";
+
+  // If apolloClient is null or undefined, create a new client.
   const _apolloClient = apolloClient ?? createApolloClient();
+  // const _apolloClient =
+  //   apolloClient !== null || apolloClient !== undefined
+  //     ? apolloClient
+  //     : createApolloClient();
+
   // If your page has Next.js data fetching methods that use Apollo Client,
   // the initial state gets hydrated here.
   if (initialState) {
+    // @TODO add cache merging here?
+
     _apolloClient.cache.restore(initialState);
   }
+
   // For SSG and SSR always create a new Apollo Client.
-  if (typeof window === "undefined") {
+  if (isServer) {
     return _apolloClient;
   }
+
   // Create the Apollo Client once in the client.
   if (!apolloClient) {
     apolloClient = _apolloClient;
   }
+
   return _apolloClient;
 }
 
