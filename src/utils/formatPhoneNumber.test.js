f4116852e676b4f826a9ad550c03865ac35b3be7
Matt Misselbeck <matt@misselbeck.com>
2022-08-30 14:42:23 -0400

    RENO-3016 Format Phone Number Utility (#302)
    * RENO-3016: formatPhoneNumber utility function
    
    * RENO-3016: Apply formatPhoneNumber utility to Location component
    
    * RENO-3016: Provide tests for formatPhoneNumber utility
    
    * RENO-3016: Conditional check no longer needed now that function accepts string or null
    
    * RENO-3016: Return null instead of empty string to match data in query
    
    Co-authored-by: Matthew Misselbeck <mam2155@med.cornell.edu>
    Co-authored-by: William Luisi <62392174+nypl-wluisi@users.noreply.github.com>

---
+++ src/utils/formatPhoneNumber.test.js
@@ -0,0 +1,37 @@
+import formatPhoneNumber from './formatPhoneNumber';
+
+describe('formatPhoneNumber', () => {
+  const unformattedPhoneData = [
+    '9172756975',
+    '(212)-752-3824',
+    '(212) 714-8400',
+    '917-275-6975',
+  ];
+
+  const expectedPhoneNumbers = [
+    '917-275-6975',
+    '212-752-3824',
+    '212-714-8400',
+    '917-275-6975',
+  ];
+  
+  const badPhoneData = [
+    '',
+    null,
+    undefined,
+    '123456789', // missing digit
+    'ABCDEFGHIJ', // 10 characters, but not numbers
+    'badDataReturnEmptyString'
+  ];
+
+  test('Phone numbers are displayed in the correct format', () => {
+    const formattedPhoneNumbers = unformattedPhoneData.map((number) => formatPhoneNumber(number));
+    expect(formattedPhoneNumbers).toEqual(expectedPhoneNumbers);
+  });
+
+  test('Any bad data should return a null', () => {
+    badPhoneData.forEach((data) => {
+      expect(formatPhoneNumber(data)).toEqual(null);
+    });
+  });
+});
