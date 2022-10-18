09cbf90fb808a5a211f969f73a93e26eed924ddc
William Luisi <williamluisi@nypl.org>
2022-10-18 17:45:58 -0400

    code cleanup

--- src/components/shared/PreviewModeNotification/PreviewModeNotification.tsx
+++ src/components/shared/PreviewModeNotification/PreviewModeNotification.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Box, Link, Notification } from "@nypl/design-system-react-components";
-// import NextDsLink from "./../Link/NextDsLink";
 
 export default function PreviewModeNotification() {
   return (
@@ -9,10 +8,6 @@ export default function PreviewModeNotification() {
       notificationHeading="You are viewing the site in preview mode."
       notificationContent={
         <Box>
-          {/* prefetch will still prefetch on hover :/ */}
-          {/* <NextDsLink prefetch={false} href="/api/exit-preview">
-            Click here to exit.
-          </NextDsLink> */}
           <Link href="/api/exit-preview">Click here to exit.</Link>
         </Box>
       }
