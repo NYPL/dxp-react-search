59384b82e0880bc9e839bbf988958ff837f93698
Matthew Misselbeck <mam2155@med.cornell.edu>
2022-11-06 17:43:55 -0500

    RENO-3178: Implement DS version of AlphabetFilter

--- src/components/online-resources/AlphabetNav/AlphabetNav.js
+++ src/components/online-resources/AlphabetNav/AlphabetNav.js
@@ -7,6 +7,7 @@ import { useQuery } from "@apollo/client";
 // @TODO move this to a shared query?
 import { AutoSuggestQuery as AUTO_SUGGEST_QUERY } from "./../SearchForm/AutoSuggest.gql";
 // Components
+import { AlphabetFilter } from "@nypl/design-system-react-components";
 import { Heading } from "@nypl/design-system-react-components";
 import s from "./AlphabetNav.module.css";
 // Utils
@@ -16,69 +17,34 @@ import getActiveLetters from "./../../../utils/getActiveLetters";
 function AlphabetNav({ className, title, description }) {
   const router = useRouter();
   // State for selected letter.
-  const selectedLetter = router.query.alpha ? router.query.alpha : null;
-  // Build alphabet array.
-  const alphabet = "abcdefghijklmnopqrstuvwxyz"
-    .split("")
-    .map((c) => c.toUpperCase());
+  const selectedLetter = router.query.alpha ? router.query.alpha.toLowerCase() : null;
   // Get the active letters from existing auto suggestion cache.
   const { data: activeLettersData } = useQuery(AUTO_SUGGEST_QUERY, {});
   const activeLetters = getActiveLetters(
     activeLettersData?.allAutoSuggestions,
     "name"
   );
+  const filteredActiveLetters = activeLetters.filter(x => isNaN(x)).map(letter => letter.toLowerCase());
+
+  function alphaLink(link) {
+    if (link === "showAll") {
+      window.location = `${ONLINE_RESOURCES_BASE_PATH}/search?alpha=all`;
+    } else {
+      window.location = `${ONLINE_RESOURCES_BASE_PATH}/search?alpha=${link.toUpperCase()}`;
+    }
+  }
 
   return (
     <>
-      <div className={`${s.AlphabetNav} ${className ? className : ""}`}>
-        <Heading id={title} level={2}>
-          <>{title}</>
-        </Heading>
-        <div className={s.description}>{description}</div>
-        <div className="letters-wrapper">
-          {alphabet.map((letter) => {
-            if (activeLetters.includes(letter)) {
-              return (
-                <Link
-                  key={letter}
-                  href={`${ONLINE_RESOURCES_BASE_PATH}/search?alpha=${letter}`}
-                >
-                  <a
-                    className={`${s.letters} ${
-                      selectedLetter === letter ? s.active : ""
-                    }`}
-                  >
-                    {letter}
-                  </a>
-                </Link>
-              );
-            } else {
-              return (
-                <a
-                  key={letter}
-                  aria-disabled="true"
-                  tabIndex={-1}
-                  className={`${s.letters} ${s.inactive}`}
-                >
-                  {letter}
-                </a>
-              );
-            }
-          })}
-          <Link
-            key={"All"}
-            href={`${ONLINE_RESOURCES_BASE_PATH}/search?alpha=all`}
-          >
-            <a
-              className={`${s.letters} ${s.all} ${
-                selectedLetter === "all" ? s.active : ""
-              }`}
-            >
-              {"Show All"}
-            </a>
-          </Link>
-        </div>
-      </div>
+      <AlphabetFilter
+        activeLetters={filteredActiveLetters}
+        className={`${s.AlphabetNav} ${className ? className : ""}`}
+        currentLetter={selectedLetter}
+        descriptionText={description}
+        headingText={title}
+        id="alphabet-filter-id"
+        onClick={(e) => alphaLink(e)}
+      />
     </>
   );
 }
