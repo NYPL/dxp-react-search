a93114d99004c31349ffe4492105de576cdc96a4
Matthew Misselbeck <mam2155@med.cornell.edu>
2022-11-20 11:13:56 -0500

    RENO-3271: ESLint + Prettier formatting

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
 import { NextPageContext } from "next";
 import { ApolloError } from "@apollo/client/errors";
-import { Button, Box, Flex, Heading, Logo, Text } from "@nypl/design-system-react-components";
+import {
+  Button,
+  Box,
+  Flex,
+  Heading,
+  Logo,
+  Text,
+} from "@nypl/design-system-react-components";
 import PageContainer from "../components/shared/layouts/PageContainer";
 const { NEXT_PUBLIC_NYPL_DOMAIN } = process.env;
 // Next
@@ -19,20 +26,22 @@ type ErrorContext = Omit<NextPageContext, "err"> & {
 
 function Error({ statusCode }: ErrorProps) {
   let title = "We're Sorry ...";
-  let description = "The page you requested is either unavailable or you need permission to view the content."
+  let description =
+    "The page you requested is either unavailable or you need permission to view the content.";
   if (statusCode === 500) {
     title = "Error!";
   }
   if (statusCode === 503) {
     title = "Site Under Maintenance";
-    description = "The New York Public Library is currently under maintenance. We should be back shortly. Thank you for your patience."
+    description =
+      "The New York Public Library is currently under maintenance. We should be back shortly. Thank you for your patience.";
   }
 
   const router = useRouter();
 
-  const handleClick = () => {  
+  const handleClick = () => {
     router.push(NEXT_PUBLIC_NYPL_DOMAIN as string);
-  }
+  };
 
   return (
     <PageContainer
@@ -55,17 +64,9 @@ function Error({ statusCode }: ErrorProps) {
       wrapperClass="nypl--404"
       contentHeader={null}
       contentPrimary={
-        <Flex
-          direction={{ sm: "column", md: "row" }}
-          alignItems="center"
-        >
-          <Box
-            overflow="hidden"
-            width={{ base: "60%", md: "50%", lg: "40%" }}
-          >
-            <Box
-              ml={{ base: 0, md: "-60px" }}
-            >
+        <Flex direction={{ sm: "column", md: "row" }} alignItems="center">
+          <Box overflow="hidden" width={{ base: "60%", md: "50%", lg: "40%" }}>
+            <Box ml={{ base: 0, md: "-60px" }}>
               <Logo
                 decorative
                 id="nypl-lion-logo"
@@ -73,15 +74,20 @@ function Error({ statusCode }: ErrorProps) {
                 size="default"
                 opacity={0.2}
               />
-            </Box>  
+            </Box>
           </Box>
-          <Box pl={{ base: "0", md: "100px" }} textAlign={{ base: "center", md: "left" }}>
-            <Heading as='h1' fontSize={{ base: '24px', md: '48px', lg: '72px' }} color="brand.primary">
+          <Box
+            pl={{ base: "0", md: "100px" }}
+            textAlign={{ base: "center", md: "left" }}
+          >
+            <Heading
+              as="h1"
+              fontSize={{ base: "24px", md: "48px", lg: "72px" }}
+              color="brand.primary"
+            >
               {title}
             </Heading>
-            <Text mb={{ base: "m", md: "xl" }}>
-              {description}
-            </Text>
+            <Text mb={{ base: "m", md: "xl" }}>{description}</Text>
             <Button
               buttonType="primary"
               id="large-primary"
