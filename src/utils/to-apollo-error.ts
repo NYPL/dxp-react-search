cd797ded7588f9533c38d20bee3f486ae775ed30
William Luisi <williamluisi@nypl.org>
2022-11-30 15:16:13 -0500

    code cleanup

--- src/utils/to-apollo-error.ts
+++ src/utils/to-apollo-error.ts
@@ -1,3 +1,7 @@
+// Copied from https://github.com/apollographql/apollo-server/blob/5352a9f57a26d65558dadc774dd406c67e40bd14/packages/apollo-server-errors/src/index.ts#L142
+// This function was removed for some reason from apollo-server v4.
+export type ApolloError = Error & { extensions: Record<string, any> };
+
 export default function toApolloError(
   error: Error & { extensions?: Record<string, any> },
   code: string = "INTERNAL_SERVER_ERROR"
@@ -8,5 +12,5 @@ export default function toApolloError(
   } else {
     err.extensions = { code };
   }
-  return err as Error & { extensions: Record<string, any> };
+  return err as ApolloError;
 }
