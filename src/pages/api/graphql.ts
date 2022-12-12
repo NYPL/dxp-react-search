51d6c3c2b78a451eeba6d68ad3fbd6682eb68ef3
William Luisi <williamluisi@nypl.org>
2022-12-12 15:43:59 -0500

    cleanup graphql.ts api and gql playground

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,12 +1,17 @@
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "@apollo/server-plugin-landing-page-graphql-playground";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+import { ApolloServerPluginLandingPageDisabled } from "@apollo/server/plugin/disabled";
 import { schema } from "../../apollo/server/schema";
 import getDataSources from "./../../apollo/server/datasources/getDataSources";
 
 const apolloServer = new ApolloServer({
   schema,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  plugins: [
+    process.env.NODE_ENV === "production"
+      ? ApolloServerPluginLandingPageDisabled()
+      : ApolloServerPluginLandingPageLocalDefault(),
+  ],
 });
 
 export default startServerAndCreateNextHandler(apolloServer, {
@@ -17,34 +22,4 @@ export default startServerAndCreateNextHandler(apolloServer, {
   },
 });
 
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
-
-// /*
-//  * There's an issue with apollo-server-micro where OPTIONS method
-//  * will return with the wrong status code.
-//  *
-//  * This only happens if you try to make a cross-origin req to the
-//  * /api/graphql route.
-//  *
-//  * @SEE https://github.com/apollographql/apollo-server/issues/2473
-//  *
-//  */
-// // Set cors policy.
-// const cors = Cors({
-//   allowMethods: ["POST", "OPTIONS"],
-//   origin: NEXT_PUBLIC_ALLOWED_ORIGIN,
-// });
-
-// const startServer = apolloServer.start();
-
-// export default cors(async (req, res) => {
-//   if (req.method === "OPTIONS") {
-//     return res.end();
-//   }
-//   await startServer;
-//   await apolloServer.createHandler({ path: "/api/graphql" })(req, res);
-// });
+// @TODO Add back Cors policy or other workaround?
