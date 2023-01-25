34e628b400832d8b6acec8f6dce68215da4b3e4a
Isabelle Stettler <70068858+isastettler@users.noreply.github.com>
2023-01-25 11:57:52 -0500

    RENO-3383: Add GA Event Tracking (#375)
    * add ga tracking to email submission
    
    * update ga placement
    
    * update gaEventLabel
    
    Co-authored-by: William Luisi <62392174+nypl-wluisi@users.noreply.github.com>

--- src/components/shared/EmailSubscription/EmailSubscription.tsx
+++ src/components/shared/EmailSubscription/EmailSubscription.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useRouter } from "next/router";
 import { Spinner } from "@chakra-ui/react";
 import EmailSubscriptionWrapper from "./EmailSubscriptionWrapper";
 import EmailSubscriptionForm from "./EmailSubscriptionForm";
@@ -16,7 +17,7 @@ interface EmailSubscriptionProps {
   formHelperText?: string;
   formPlaceholder?: string;
   salesforceListId?: number;
-  salesforceSourceCode?: string;
+  salesforceSourceCode: string;
 }
 
 export default function EmailSubscription({
@@ -38,9 +39,16 @@ export default function EmailSubscription({
   const [isSubmitting, setIsSubmitting] = React.useState(false);
 
   const [status, setStatus] = React.useState<StatusCode>();
+  const { asPath } = useRouter();
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
+
+    //Create dynamic Google Analytics values
+    const gaEventCategory = "Email Subscription Forms";
+    const gaEventActionName = `Subscribe - ${asPath}`; // example: Subscribe - /research
+    const gaEventLabel = `Success ${salesforceSourceCode}`; // example: Success research
+
     setIsSubmitting(true);
     if (formBaseUrl !== undefined) {
       // API endpoint where we send form data.
@@ -62,11 +70,20 @@ export default function EmailSubscription({
       try {
         const response = await fetch(endpoint, options);
         const result = await response.json();
+
+        // @ts-ignore
+        window.gtag("event", gaEventActionName, {
+          event_category: gaEventCategory,
+          event_label: gaEventLabel,
+        });
+
         setStatus(result.statusCode);
         setIsSubmitted(true);
       } catch (error) {
         setStatus("ERROR");
         setIsSubmitted(true);
+        /* @TODO maybe it would be usful to submit a 
+        GA event with label "Error - ${salesforceSourceCode}"?*/
       }
       setIsSubmitting(false);
     }
