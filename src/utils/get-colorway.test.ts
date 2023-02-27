1e0828c2253a0f0eaeb15aa291dbfb0f8c4eaa56
Isabelle Stettler <70068858+isastettler@users.noreply.github.com>
2023-02-27 14:05:34 -0500

    RENO-3435: Add String Sanitization To GetColorway (#391)
    * add string sanitation for slugs to colorway
    
    * fix typo
    
    * update tests for get-colorway

--- src/utils/get-colorway.test.ts
+++ src/utils/get-colorway.test.ts
@@ -1,7 +1,7 @@
 import getColorway from "./get-colorway";
 
 describe("getColorway", () => {
-  it("should return correct colorway for /give slug.", () => {
+  it("should return the correct colorway for /give slug.", () => {
     const mockColorway = {
       primary: "brand.primary",
       secondary: "brand.secondary",
@@ -9,19 +9,51 @@ describe("getColorway", () => {
     expect(getColorway("/give")).toEqual(mockColorway);
   });
 
-  it("should return correct colorway for /research slug.", () => {
+  it("should return the correct colorway when a slug starting with '/research' or the string 'research' was passed.", () => {
     const mockColorway = {
       primary: "section.research.primary",
       secondary: "section.research.secondary",
     };
     expect(getColorway("/research")).toEqual(mockColorway);
+    expect(getColorway("/research/support")).toEqual(mockColorway);
+    expect(getColorway("/research/collections")).toEqual(mockColorway);
+    // Works when the string "research" is passed
+    expect(getColorway("research")).toEqual(mockColorway);
   });
 
-  it("should return correct colorway for a section.", () => {
+  it("should return the correct colorway when a slug starting with '/education' or the string 'education' was passed.", () => {
+    const mockColorway = {
+      primary: "#2540A4",
+      secondary: "#1D62E6",
+    };
+    expect(getColorway("/education")).toEqual(mockColorway);
+    expect(getColorway("/education/educators")).toEqual(mockColorway);
+    expect(getColorway("/education/kids")).toEqual(mockColorway);
+    expect(getColorway("/education/adults/career-employment")).toEqual(
+      mockColorway
+    );
+    // Works when the string "education" is passed
+    expect(getColorway("education")).toEqual(mockColorway);
+  });
+
+  it("should return the correct colorway for 'section_front'.", () => {
     const mockColorway = {
       primary: "brand.primary",
       secondary: "brand.secondary",
     };
     expect(getColorway("section_front")).toEqual(mockColorway);
+  });
+
+  it("should return the default colorway for any string that does not match a specific colorway.", () => {
+    const defaultColorway = getColorway("default");
+    const mockColorway = {
+      primary: "brand.primary",
+      secondary: "brand.secondary",
+    };
+    expect(defaultColorway).toEqual(mockColorway);
+    expect(getColorway("generic-section-front")).toEqual(mockColorway);
+    // Default values are returend if the prefix "/" is missing from a slug
+    expect(getColorway("research/collections")).toEqual(mockColorway);
+    expect(getColorway("education/educators")).toEqual(mockColorway);
   });
 });
