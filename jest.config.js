55a625c45921a8feb285fb72b3b2cf9e0709ff8f
Isa Stettler <stettlerisabelle@gmail.com>
2023-04-20 13:32:33 -0400

    remove babelrc, update jest.config, update package.json

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,22 @@
-module.exports = {
-  collectCoverageFrom: ["**/*.{js,jsx,tsx}", "!**/node_modules/**"],
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const config = {
+  // Add more setup options before each test is run
   setupFiles: ["<rootDir>/jest.setupFiles.js"],
   setupFilesAfterEnv: [
     "<rootDir>/testHelper/set-env-vars.js",
-    "<rootDir>/testHelper/browser.js",
     "<rootDir>/jest.setupFiles.js",
     "react-intersection-observer/test-utils",
   ],
-  testPathIgnorePatterns: ["/node_modules/", "/.next/", "/cypress/"],
-  transform: {
-    "^.+\\.(js|jsx)$": "<rootDir>/node_modules/babel-jest",
-    "\\.(gql|graphql)$": "jest-transform-graphql",
-    ".*": "babel-jest",
-  },
-  transformIgnorePatterns: [
-    "<rootDir>/node_modules/",
-    "^.+\\.module\\.(css|sass|scss)$",
-  ],
-  moduleNameMapper: {
-    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
-  },
+  testEnvironment: "jest-environment-jsdom",
+  testPathIgnorePatterns: ["/cypress/"],
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(config);
