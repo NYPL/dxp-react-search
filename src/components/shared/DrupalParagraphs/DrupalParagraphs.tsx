f0cfb873fa20a3f95830c82bfac3d514da1e89ab
Isa Stettler <stettlerisabelle@gmail.com>
2023-10-20 11:58:18 -0400

    update DrupalParagraphs

--- src/components/shared/DrupalParagraphs/DrupalParagraphs.tsx
+++ src/components/shared/DrupalParagraphs/DrupalParagraphs.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export interface DrupalParagraphs<T> {
   /** The content for a paragraphs field. An array of objects. */
-  content: DrupalParagraphsItem[];
+  content: DrupalParagraphsItem | DrupalParagraphsItem[];
   /** A map of components, keyed by type name. */
   components: T;
 }
@@ -16,20 +16,33 @@ export default function DrupalParagraphs<T extends Record<string, any>>({
   content,
   components,
 }: DrupalParagraphs<T>) {
-  return (
-    <>
-      {/* @TODO This should also be able to handle a single value paragraphs field */}
-      {content &&
-        content.map((item: DrupalParagraphsItem) => {
-          if (typeof components[item["__typename"]] !== "undefined") {
-            const componentType = components[item["__typename"]];
+  // In the case where the query returned null for a page section
+  if (content === null) return null;
+  if (content) {
+    if (Array.isArray(content)) {
+      return (
+        <>
+          {content.map((item: DrupalParagraphsItem) => {
+            if (typeof components[item["__typename"]] !== "undefined") {
+              const componentType = components[item["__typename"]];
 
-            return React.createElement(componentType, {
-              key: `${item.__typename}-${item.id}`,
-              ...item,
-            });
-          }
-        })}
-    </>
-  );
+              return React.createElement(componentType, {
+                key: `${item.__typename}-${item.id}`,
+                ...item,
+              });
+            }
+          })}
+        </>
+      );
+    }
+    if (typeof components[content["__typename"]] !== "undefined") {
+      const componentType = components[content["__typename"]];
+      return React.createElement(componentType, {
+        key: `${content.__typename}-${content.id}`,
+        ...content,
+      });
+    }
+  }
+  console.warn("Content type not supported in DrualParagraph: ", content);
+  return null;
 }
