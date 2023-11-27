42cae9311901775824712c10885a886e7c974d08
William Luisi <williamluisi@nypl.org>
2023-11-27 14:23:48 -0500

    code cleanup

--- src/apollo/server/resolvers/drupal-paragraphs/get-drupal-paragraphs-field.ts
+++ src/apollo/server/resolvers/drupal-paragraphs/get-drupal-paragraphs-field.ts
@@ -11,25 +11,52 @@ export interface DrupalJsonApiField extends DrupalJsonApiEntityResource {
 export function getDrupalParagraphsField<
   T extends DrupalJsonApiField | DrupalJsonApiEntityResource[]
 >(field: T) {
-  const isMultiValueField = Array.isArray(field) ? true : false;
-  // Multi value fields will return an empty array, `[]` if no paragraphs are added.
+  // The `data` property will only be present in response, if there is no content added to the field.
   if ("data" in field) {
+    // Multi value fields will return an empty array, `[]` if no paragraphs are added.
+    //
+    // @example
+    // "field_main_content": {
+    //   "data": [],
+    // }
     if (field.data?.length === 0) {
       return null;
     }
-    // Single value fields will return null, `null` if no paragraph is added.
+
+    // Single value fields will return `null` if no paragraph is added.
+    //
+    // @example
+    // "field_ers_featured": {
+    //   "data": null,
+    // }
     if (field.data === null) {
       return null;
     }
   }
 
-  // Filter out unpublished paragraphs, for multi value field only.
+  // Only a multivalue field will return an array.
   if (Array.isArray(field)) {
-    console.log("isMultiValueField", isMultiValueField);
-
-    return field.filter((paragraphItem: DrupalJsonApiEntityResource) =>
-      paragraphItem.hasOwnProperty("status")
+    // Filter out unpublished paragraphs, for multi value field only.
+    const fieldFinal = field.filter(
+      (paragraphItem: DrupalJsonApiEntityResource) =>
+        paragraphItem.hasOwnProperty("status")
     );
+
+    // Add check for multivalue field, with 1 value that's unpublished ?
+    //
+    // @example
+    // "field_main_content": [
+    //   {
+    //     "type": "paragraph--text",
+    //     "id": "93494979-141a-4e27-8bbc-b06a729f3e5c",
+    //   }
+    // ]
+    // @todo figure out if this is worth doing ? Without this, it will just return an empty array, `[]`.
+    if (fieldFinal.length === 0) {
+      return null;
+    }
+
+    return fieldFinal;
   }
 
   return field;
