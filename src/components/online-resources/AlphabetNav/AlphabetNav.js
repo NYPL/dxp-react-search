7bd8ec568586aa9adc6083dd6b4846b946641d45
William Luisi <williamluisi@nypl.org>
2024-03-27 17:33:36 -0400

    temp change a-z filters back to router.push to test bug

--- src/components/online-resources/AlphabetNav/AlphabetNav.js
+++ src/components/online-resources/AlphabetNav/AlphabetNav.js
@@ -31,14 +31,14 @@ function AlphabetNav({ className, title, description }) {
   const handleClick = (link) => {
     if (link === "showAll") {
       // @note This is a temporary fix until the imperva WAF is more stable and stops blocking nextjs client side routing.
-      // router.push(`${ONLINE_RESOURCES_BASE_PATH}/search?alpha=all`);
-      window.location.href = `${ONLINE_RESOURCES_BASE_PATH}/search?alpha=all`;
+      router.push(`${ONLINE_RESOURCES_BASE_PATH}/search?alpha=all`);
+      // window.location.href = `${ONLINE_RESOURCES_BASE_PATH}/search?alpha=all`;
     } else {
       // @note This is a temporary fix until the imperva WAF is more stable and stops blocking nextjs client side routing.
-      // router.push(
-      //   `${ONLINE_RESOURCES_BASE_PATH}/search?alpha=${link.toUpperCase()}`
-      // );
-      window.location.href = `${ONLINE_RESOURCES_BASE_PATH}/search?alpha=${link.toUpperCase()}`;
+      router.push(
+        `${ONLINE_RESOURCES_BASE_PATH}/search?alpha=${link.toUpperCase()}`
+      );
+      // window.location.href = `${ONLINE_RESOURCES_BASE_PATH}/search?alpha=${link.toUpperCase()}`;
     }
   };
 
